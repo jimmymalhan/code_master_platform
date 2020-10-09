9b021bccf42f2b64fac8ebf88044a8b171dbddb7
Jimmy Malhan <jmalhan@medqia.com>
2020-10-09 16:11:17 -0700

    adding division

---
+++ hacker_rank/division.py
@@ -0,0 +1,15 @@
+# Task
+# The provided code stub reads two integers,  and , from STDIN.
+
+# Add logic to print two lines. The first line should contain the result of integer division,  a//b . The second line should contain the result of float division,  a/b .
+
+# No rounding or formatting is necessary.
+
+# The result of the integer division 3//5 = 0.
+# The result of the float division is 3/5 = 0.6.
+
+a = 4
+b = 3
+
+if __name__ == "__main__":
+    print("{0}\n{1}".format(a//b, a/b))
