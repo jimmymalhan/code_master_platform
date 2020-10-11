3e1670f7a91d76263e805eba1844ba841b77dddf
Jimmy Malhan <jimmymalhan@gmail.com>
2020-10-10 19:02:25 -0700

    adding more data for Q&A

--- Answers/hacker_rank/Introduction/loops.py
+++ Answers/hacker_rank/Introduction/loops.py
@@ -1,8 +1,16 @@
-# Task
-# The provided code stub reads and integer,n, from STDIN. For all non-negative integers i < n, print i**.
+# The provided code stub reads and integer,n, from STDIN. For all non-negative integers i < n, print i^2.
 
 n = 3
 
+# The list of non-negative integers that are less than n = 3 is [0,1,2]. Print the square of each number on a separate line.
+# Output
+# 0
+# 1
+# 4
+
 if __name__ == '__main__':
-    for i in range(n):
-        print(i**2)
+    # for i in range(n):
+    #     print(i**2)
+
+#or
+    [print (i**2) for i in range (n)]
--- Answers/hacker_rank/Introduction/print_function.py
+++ Answers/hacker_rank/Introduction/print_function.py
@@ -1,25 +1,28 @@
-# The included code stub will read an integer, n, from STDIN.
-# Without using any string methods, try to print the following:
-# 123...n
+# The included code stub will read an integer, n, from STDIN. Without using any string methods, try to print the following:
+#       123...n
 # Note that "" represents the consecutive values in between.
+
 # Example
+# n = 5
 # Print the string 12345.
+
 # Input Format
 # The first line contains an integer n.
+
 # Constraints
 # 1<=n<=150
+
 # Output Format
-# Print the list of integers 1 from  through  n as a string, without spaces.
+# Print the list of integers 1 from through n as a string, without spaces.
+
+# Sample input 
+# n = 3
+# sample Output = 123
 
 from __future__ import print_function
 
 n = 3
 
 if __name__ == "__main__":
-    # n = int(input())
-    # for i in range(1 , n + 1):
-    #     print(i, end='')
-
-#or
-
-    print(*range(1, int(input())+ 1), sep='')
+    for i in range(1, n+1):
+        print(i, end='')
