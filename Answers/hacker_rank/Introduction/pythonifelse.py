566aa711e45fe1ce438027be4af8de4349b7b140
Jimmy Malhan <jimmymalhan@gmail.com>
2020-10-10 18:34:00 -0700

    rearranging_Q&A

--- Answers/hacker_rank/Introduction/pythonifelse.py
+++ Answers/hacker_rank/Introduction/pythonifelse.py
@@ -1,6 +1,25 @@
-n = 19
+# Given an integer, n, perform the following conditional actions:
 
-if (n in range (6, 21) or n % 2 != 0):
-    print('Weird')
-else:
-    print('Not Weird')
+# If n is odd, print Weird <-
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird <-
+# If n is even and greater than 20, print Not Weird
+
+# Test Cases:
+# n = 3 -> Weird
+# n = 24 -> Not Weird
+
+#!/bin/python3
+
+import os
+import random
+import re
+import sys
+
+n = 24
+
+if __name__ == '__main__':
+    if (n % 2 != 0 or n in range(6, 21)):
+        print("Weird")
+    else:
+        print('Not weird')  
