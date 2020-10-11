61c373dd727d77aa008b83aebf0adcc3e5c41c3b
Jimmy Malhan <jimmymalhan@gmail.com>
2020-10-10 17:43:07 -0700

    adding

---
+++ Answers/hacker_rank/Introduction/print_function.py
@@ -0,0 +1,25 @@
+# The included code stub will read an integer, n, from STDIN.
+# Without using any string methods, try to print the following:
+# 123...n
+# Note that "" represents the consecutive values in between.
+# Example
+# Print the string 12345.
+# Input Format
+# The first line contains an integer n.
+# Constraints
+# 1<=n<=150
+# Output Format
+# Print the list of integers 1 from  through  n as a string, without spaces.
+
+from __future__ import print_function
+
+n = 3
+
+if __name__ == "__main__":
+    # n = int(input())
+    # for i in range(1 , n + 1):
+    #     print(i, end='')
+
+#or
+
+    print(*range(1, int(input())+ 1), sep='')
