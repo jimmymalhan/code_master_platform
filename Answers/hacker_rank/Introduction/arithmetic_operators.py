e5b0bb5bfc76f1c7ac1e08e89b9bfdc83cb2cd8b
Jimmy Malhan <jimmymalhan@gmail.com>
2020-10-10 18:34:00 -0700

    rearranging_Q&A

--- Answers/hacker_rank/Introduction/arithmetic_operators.py
+++ Answers/hacker_rank/Introduction/arithmetic_operators.py
@@ -1,18 +1,26 @@
-# Task
 # The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
 
 # The first line contains the sum of the two numbers.
 # The second line contains the difference of the two numbers (first - second).
 # The third line contains the product of the two numbers.
+
+
+# Explanation
+# 3 + 2 = 5
+# 3 - 2 = 1
+# 3 * 2 = 6
+
 a = 3
 b = 2
 
-# if __name__ == "__main__":
-#     print(a + b)
-#     print(a - b)
-#     print(a * b)
-
-# or
-
 if __name__ == "__main__":
-    print('{0} \n{1} \n{2}'.format((a + b), (a - b), (a * b)))
+    # print(a+b)
+    # print(a-b)
+    # print(a*b)
+
+#or
+    # for list in a,b:
+    #     print((a+b), (a-b), (a*b))
+    #     break
+#or
+    print('{0} \n{1} \n{2}'.format((a+b), (a-b), (a*b)))
