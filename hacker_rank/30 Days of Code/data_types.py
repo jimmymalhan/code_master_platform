88f8f1928df9918225628f975e3300320fdd4f86
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-12 18:00:15 -0700

    commit

---
+++ hacker_rank/30 Days of Code/data_types.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a = 0
+b = 0.0
+c = ""
+
+# Read and save an integer, double, and String to your variables.
+a = int(input())
+b = float(input())
+c = str(input())
+# Print the sum of both integer variables on a new line.
+print(a+i)
+print(b+d)
+print(s+c)
---
+++ hacker_rank/30 Days of Code/operators.py
@@ -0,0 +1,26 @@
+# mC = float(input())
+# tipPercent = 20
+# taxPercent = 8
+# tip = mC * 20 / 100
+# tax = mC * 8 / 100
+# totalCost  = int(mC + tip + tax)
+# print(totalCost)
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = (meal_cost * tip_percent) / 100
+    tax = (meal_cost * tax_percent) / 100
+    totalCost = int(round(meal_cost + tip + tax))
+    print(totalCost)
+if __name__ == '__main__':
+    meal_cost = 12.0
+    tip_percent = 20
+    tax_percent = 8
+
+    print(solve(meal_cost, tip_percent, tax_percent))
