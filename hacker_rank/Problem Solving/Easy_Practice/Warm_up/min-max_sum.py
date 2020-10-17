89d978294338fd411f19b6a9d8f8ad8aef22c468
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-17 10:53:14 -0700

    commit

---
+++ hacker_rank/Problem Solving/Easy_Practice/Warm_up/min-max_sum.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    x = sum(arr)
+    print(x-(max(arr)), x-(min(arr)))
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
