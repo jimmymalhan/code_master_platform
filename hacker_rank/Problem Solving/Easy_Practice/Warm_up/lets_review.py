1cd24f1149deadc71643b9a611c1192830e3ca2a
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-17 14:21:20 -0700

    commit

--- hacker_rank/Problem Solving/Easy_Practice/Warm_up/lets_review.py
+++ hacker_rank/Problem Solving/Easy_Practice/Warm_up/lets_review.py
@@ -1,1 +1,6 @@
-for i in range(int(input())): s=input(); print(*["".join(s[::2]),"".join(s[1::2])])
+for i in range(int(input())):
+    s = input()
+    print(*["".join(s[::2]), "".join(s[1::2])])
+
+or
+#for i in range(int(input())): s=input(); print(*["".join(s[::2]),"".join(s[1::2])])
