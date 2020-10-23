fa20d3bf5b0702adbb852a95c30d6d187b425ff4
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-22 20:12:21 -0700

    commit

---
+++ hacker_rank/30 Days of Code/013_abstract_classes.py
@@ -0,0 +1,21 @@
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+
+#Write MyBook clas:
+
+def __init__(self, title, author, price):
+    self.title = title
+    self.author = author
+    self.price = price
+
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
