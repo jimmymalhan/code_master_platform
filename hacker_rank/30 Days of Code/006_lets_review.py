582d3ff9f64e7d934b1822175e4c115157ca2930
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-24 14:43:47 -0700

    commit

--- hacker_rank/30 Days of Code/006_lets_review.py
+++ hacker_rank/30 Days of Code/006_lets_review.py
@@ -1,6 +1,31 @@
-for i in range(int(input())):
-    s = input()
-    print(*["".join(s[::2]), "".join(s[1::2])])
+# S = string of length N > INDEXED 0 to N-1
+#print even-INDEXED and odd-INDEXED characters 
+# as 2 space-separated strings on a single line
 
-or
-#for i in range(int(input())): s=input(); print(*["".join(s[::2]),"".join(s[1::2])])
+# Constraints
+# 1 <= T <= 10
+# 2 <= length of S <= 10000
+
+# Sample Input 
+# 2
+# Hacker
+# Rank
+
+#Sample Output
+# Hce akr
+# Rn ak
+
+num_test_cases = int(input())
+
+for i in range(num_test_cases):
+    test_string = input()
+    even_indexed_characters = ''
+    odd_indexed_characters = ''
+
+    for j in range(len(test_string)):
+        if j % 2 == 0:
+            even_indexed_characters += test_string[j]
+        else:
+            odd_indexed_characters += test_string[j]
+    
+    print('{} {}'.format(even_indexed_characters, odd_indexed_characters))
