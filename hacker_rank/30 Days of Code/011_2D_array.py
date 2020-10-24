db5f6a4c64f9de4a608a0e8ec9e071666a23ddac
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-24 05:27:56 -0700

    commit

--- hacker_rank/30 Days of Code/011_2D_array.py
+++ hacker_rank/30 Days of Code/011_2D_array.py
@@ -1,13 +1,38 @@
-def hourglasses(arr, i, j):
-    return sum(arr[i][j:j+3]) + arr[i+1][j+1] + sum(arr[i+2][j:j+3])
+#Input
+# 1 1 1 0 0 0
+# 0 1 0 0 0 0
+# 1 1 1 0 0 0
+# 0 0 2 4 4 0
+# 0 0 0 2 0 0
+# 0 0 1 2 4 0
 
+#Output
+# 19
 
+#Constraints
+# -9 <= A[i][j] <= 9
+# 0 <= i, j <= 5
+
+def get_hourglass_sum(matrix, row, col):
+    sum = 0
+    sum += matrix[row-1][col-1]
+    sum += matrix[row-1][col]
+    sum += matrix[row-1][col+1]
+    sum += matrix[row][col]
+    sum += matrix[row+1][col-1]
+    sum += matrix[row+1][col]
+    sum += matrix[row+1][col+1]
+    return sum
+    
 arr = []
 for i in range(6):
     arr.append(list(map(int, input().split())))
 
-ans = hourglasses(arr, 0, 0)
-for i in range(4):
-    for j in range(4):
-        ans = max(ans, hourglasses(arr, i, j))
-print(ans)
+max_hourglass_sum = -63 # sum from - 9 to 9 - > constraints 
+for i in range(1,5):
+    for j in range(1,5):
+        current_hourglass_sum = get_hourglass_sum(arr, i ,j)
+        if current_hourglass_sum > max_hourglass_sum:
+            max_hourglass_sum = current_hourglass_sum
+
+print(max_hourglass_sum)
