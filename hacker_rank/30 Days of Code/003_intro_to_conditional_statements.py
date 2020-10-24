ef5fbbc56e6637a609398a5ce9102086c9a1693d
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-24 13:37:18 -0700

    commit

--- hacker_rank/30 Days of Code/003_intro_to_conditional_statements.py
+++ hacker_rank/30 Days of Code/003_intro_to_conditional_statements.py
@@ -1,12 +1,3 @@
-import math
-import os
-import random
-import re
-import sys
-
-
-
-if __name__ == '__main__':
     N = 24
     if N % 2 != 0 or (N % 2 == 0 and N in (range(6, 21))):
         print('Weird')
--- hacker_rank/30 Days of Code/004_class_vs_instance.py
+++ hacker_rank/30 Days of Code/004_class_vs_instance.py
@@ -1,5 +1,17 @@
+# If Argument for intialAge is negative > constructor should set to 0
+# Year passed() age increased by 1
+# For amIold():
+# if age < 13, print you are young.
+# if age >= 13 and age < 18 print You are a teenager
+# Otherwise, print You are old 
+
+#Constraints
+# 1 =< T <= 4
+# -5 <= age <= 30
+
 class Person:
     def __init__(self, initialAge):
+# Add some more code to run some checks on initialAge
         if initialAge < 0:
             print('Age is not valid, setting age to 0.')
             self.initialAge = 0
@@ -7,6 +19,7 @@ class Person:
             self.initialAge = initialAge
 
     def amIOld(self):
+# Do some computations in here and print out the correct statement to the console
         if self.initialAge < 13:
             print('You are young.')
         elif self.initialAge < 18:
@@ -15,14 +28,5 @@ class Person:
             print('You are old.')
 
     def yearPasses(self):
+# Increment the age of the person in here
         self.initialAge += 1
-
-t = int(input())
-for i in range(0, t):
-    age = int(input())         
-    p = Person(age)  
-    p.amIOld()
-    for j in range(0, 3):
-        p.yearPasses()       
-    p.amIOld()
-    print("")
