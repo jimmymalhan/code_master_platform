d479af650fd2540392d65381c03ab6092d519c30
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-24 13:37:18 -0700

    commit

--- hacker_rank/30 Days of Code/002_operators.py
+++ hacker_rank/30 Days of Code/002_operators.py
@@ -6,12 +6,6 @@
 # totalCost  = int(mC + tip + tax)
 # print(totalCost)
 
-import math
-import os
-import random
-import re
-import sys
-
 # Complete the solve function below.
 def solve(meal_cost, tip_percent, tax_percent):
     tip = (meal_cost * tip_percent) / 100
