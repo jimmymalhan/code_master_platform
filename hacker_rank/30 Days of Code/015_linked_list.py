3f6c6220e23f45e3daffb6c6d6600106b4c6d33f
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-24 18:30:01 -0700

    commit

---
+++ hacker_rank/30 Days of Code/015_linked_list.py
@@ -0,0 +1,47 @@
+# Singly linked list
+# Complete the insert function -> increases the new Node
+# (pass data as the Node constructor argument)
+# and insert it to the tail of the linked list reference by the head parameter.
+# Once the new node is added, return the reference to the head node
+
+# Note: If the head argument passed to the insert function is null, then the inital list is empty
+
+# sample input
+# 4
+# 2
+# 3
+# 4
+# 1
+
+#sample output
+# 2 3 4 1
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        newNode = Node(data)
+        if not head:
+            return newNode
+        current = head
+        while current.next:
+            current = current.next
+        current.next = newNode
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
