f2446ce31f412f7685763c02e88a0bd2280ce7fc
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-25 07:30:54 -0700

    commit

--- hacker_rank/Practice/Basic Data Types/001_find_the_runner-up_score.py
+++ hacker_rank/Practice/Basic Data Types/001_find_the_runner-up_score.py
@@ -1,3 +1,16 @@
+#Find the score of runner up
+
+#Constraints
+# 2 <=n <= 10
+# -100 <= A[i]
+
+# Sample input
+# 5
+# 2 3 6 6 5
+
+# Sample Output
+# 5
+
 i = int(input())
 lis = list(map(int,input().strip().split())) #generates the list
 z = max(lis) # find the maximum value in the list
--- hacker_rank/Practice/Basic Data Types/003_finding_the_percentage.py
+++ hacker_rank/Practice/Basic Data Types/003_finding_the_percentage.py
@@ -1,11 +1,35 @@
-if __name__ == '__main__':
-    n = int(input())
-    student_marks = {}
-    for _ in range(n):
-        name, *line = input().split()
-        scores = list(map(float, line))
-        student_marks[name] = scores
-    query_name = input()
-    # enter your code here
-    query_scores = student_marks[query_name]
-    print('{0:.2f}'.format(sum(query_scores)/len(query_scores)))
+# marks key:value pairs
+#'alpha':[20, 30, 40]
+#'beta':[30, 50, 70]
+# query_name = 'beta'
+# query_name in beta average score (30 + 50 + 70)/3 = 50.0
+
+# Constraints:
+# 2 <= n <= 10
+# 0 <= marks[i] <= 100
+# length of marks arrays = 3
+
+#Output
+# print one line that average of the marks obtained 
+# by the particular student correct to 2 decimal places
+
+# Sample input:
+# 3
+# Krishna 67 68 69
+# Arjun 70 98 63
+# Malika 52 56 60
+# Malika
+
+# Sample Output
+# 56.00
+
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+# enter your code here
+query_scores = student_marks[query_name]
+print('{0:.2f}'.format(sum(query_scores)/len(query_scores)))
