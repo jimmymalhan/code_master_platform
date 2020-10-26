9cf2949d225b439f4fe522fb28cafddd2030f1ca
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-25 18:03:07 -0700

    commit

--- hacker_rank/Algorithms/001_Warm_up/003_compare_the_triplets.py
+++ hacker_rank/Algorithms/001_Warm_up/003_compare_the_triplets.py
@@ -1,6 +1,10 @@
 # a = (a[0], a[1], a[2]) = (5,6,7) #alice
 # b = (b[0], b[1], b[2]) = (3,6,10) # bob
 
+# If a[i] > b[i], then Alice is awarded 1 point.
+# If a[i] < b[i], then Bob is awarded 1 point.
+# If a[i] = b[i], then neither person receives a point. 
+
 # a[0] > b[0], so Alice receives 1 point
 # a[1] = b[1], so nobody receives a point
 # a[2] < b[2], so Bob receives 1 point
--- hacker_rank/Algorithms/001_Warm_up/006_plus_minus.py
+++ hacker_rank/Algorithms/001_Warm_up/006_plus_minus.py
@@ -1,6 +1,37 @@
+# For array of integers, calculate the ratios of its elements that are positive,
+# negative, zero. print the decimal value of each fraction on a new line with 6 place
+# after the decimal
+
+# arr = [1, 1 , 0 , -1, -1]
+# n = 5
+# the ratios are:
+# 2/5 = 0.4000000
+# 2/5 = 0.4000000
+# 1/5 = 0.2000000
+
+# Constraints
+# 0 < n <= 100
+# -100 <= arr[i] <= 100
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
 def plusMinus(arr):
-    positive = sum(x > 0 for x in arr) / len(arr)
-    negative = sum(x < 0 for x in arr) / len(arr)
-    zero = sum(x == 0 for x in arr) / len(arr)
+    p, n, z  = 0,0,0
+    for i in range(len(arr)):
+        p = sum(x > 0 for x in arr) / len(arr) 
+        n = sum(x < 0 for x in arr) / len(arr) 
+        z = sum(x == 0 for x in arr) / len(arr) 
+
+    print(p, n, z, sep='\n')
     
-    print(positive, negative, zero, sep="\n")
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
