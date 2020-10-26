406f805e6712518ac130ed85fb7eea1b9a241db3
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-25 20:02:15 -0700

    commit

--- hacker_rank/Algorithms/002_Implementation/002_number_line_jumps.py
+++ hacker_rank/Algorithms/002_Implementation/002_number_line_jumps.py
@@ -1,3 +1,9 @@
+# Kangaroo 1 starts at x1 = 2 with a jump distance v1 = 1
+# and kangroo 2 starts at x2 = 1 with a jump distance v2 = 2
+# after one jump, they  are both ar x = 3,
+# (x1 + v1 = 2 + 1, x2 + v2 = 1 + 2)
+# so our answer is YES
+
 def kangaroo(x1, v1, x2, v2):
     return 'YES' if (v1 > v2) and (x2 - x1) % (v2 - v1) == 0 else 'NO'
 
--- hacker_rank/Algorithms/002_Implementation/003_between_two_sets.py
+++ hacker_rank/Algorithms/002_Implementation/003_between_two_sets.py
@@ -1,3 +1,24 @@
+# arrays a = [2, 6] and b = [24,36],
+# there are two numbers between them 6 and 12
+# 6 % 2 = 0, 6 % 6 = 0, 36 % 6 = 0 for the first value
+# Similarly, 12 % 2 = 0, 12 % 6 = 0 and 24 % 12 = 0.
+# 36 % 12 = 0
+
+# Sample input
+# 2 3
+# 2 4
+# 16 32 96
+
+# Output
+# 3
+
+# Explanation
+# 2 and 4 divide evenly into 4, 8, 12 and 16.
+# 4, 8 and 16 divide evenly into 16, 32, 96.
+# 4, 8 and 16 are the only three numbers for which each element 
+# of a is a factor and each is a factor of all elements of b.
+
+
 import sys
 from math import gcd
 from functools import reduce
