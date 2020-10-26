dd16664dba802f67f016b1c2efc1606e09693c55
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-25 20:02:15 -0700

    commit

--- hacker_rank/Algorithms/002_Implementation/001_appleandorange.py
+++ hacker_rank/Algorithms/002_Implementation/001_appleandorange.py
@@ -1,3 +1,17 @@
+# It should print the number of apples and oranges that land on Sam's house, 
+# each on a separate line.
+
+# a---d----->  <-s----house-----t> <--d--- b 
+# a = apple tree
+# d = distance
+# b = orange tree
+# countApplesAndOranges has the following parameter(s):
+# s: integer, starting point of Sam's house location.
+# t: integer, ending location of Sam's house location.
+# a: integer, location of the Apple tree.
+# b: integer, location of the Orange tree.
+# apples: integer array, distances at which each apple falls from the tree.
+# oranges: integer array, distances at which each orange falls from the tree.
 
 import math
 import os
@@ -12,25 +26,14 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
 
 if __name__ == '__main__':
     st = input().split()
-
     s = int(st[0])
-
     t = int(st[1])
-
     ab = input().split()
-
     a = int(ab[0])
-
     b = int(ab[1])
-
     mn = input().split()
-
     m = int(mn[0])
-
     n = int(mn[1])
-
     apples = list(map(int, input().rstrip().split()))
-
     oranges = list(map(int, input().rstrip().split()))
-
     countApplesAndOranges(s, t, a, b, apples, oranges)
--- hacker_rank/Algorithms/002_Implementation/004_breaking_the_records.py
+++ hacker_rank/Algorithms/002_Implementation/004_breaking_the_records.py
@@ -1,3 +1,22 @@
+# #                                  Count
+# Game  Score  Minimum  Maximum   Min Max
+#  0      12     12       12       0   0
+#  1      24     12       24       0   1
+#  2      10     10       24       1   1
+#  3      24     10       24       1   1
+# Given the scores for a season, find and 
+# print the number of times Maria breaks her records 
+# for most and least points scored during the season.
+
+# Sample Input
+# 9
+# 10 5 20 20 4 5 2 25 1
+
+# Sample Output
+# 2 4
+# https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+
+
 def breakingRecords(scores):
     min = max = scores[0]
     min_count = max_count = 0
