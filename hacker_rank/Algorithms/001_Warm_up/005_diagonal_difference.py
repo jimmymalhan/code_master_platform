ada40ddc654d3c65a2e2e3932a88e0b4359dcc26
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-25 18:03:07 -0700

    commit

--- hacker_rank/Algorithms/001_Warm_up/005_diagonal_difference.py
+++ hacker_rank/Algorithms/001_Warm_up/005_diagonal_difference.py
@@ -1,3 +1,25 @@
+# Given a square matrix, calculate the 
+# absolute difference between the sums of its diagonals.
+
+# For example, the square matrix  is shown below:
+
+# 1 2 3
+# 4 5 6
+# 9 8 9  
+
+# the left to right diaginal = 1 + 5 + 9 = 15
+# the right to left diagonal = 3 + 5 + 9 = 17
+# their absolute difference is |15 - 17| = 2
+
+# Constraints:
+# - 100 <= arr[i][j] <= 100
+
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+
+
 import math
 import os
 import random
@@ -5,13 +27,20 @@ import re
 import sys
 
 
-n = 3
-a = [[11, 2, 4], ]
-
 def diagonalDifference(arr):
     l, r = 0, 0
-    # single pass, grab both values
-    for i in range(len(arr)):
+    for i in (range(len(arr))):
         l += arr[i][i]
         r += arr[i][-i-1]
     return abs(l-r)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+    fptr.write(str(result) + '\n')
+    fptr.close()
--- hacker_rank/Algorithms/001_Warm_up/007_staircase.py
+++ hacker_rank/Algorithms/001_Warm_up/007_staircase.py
@@ -1,2 +1,25 @@
-for i in range(1, n+1):
-    print (" "*(n-i) + "#"*i)
+# Staircase
+# For staircase of size n = 4
+
+   #
+  ##
+ ###
+####
+
+# Constraints:
+# 0 < n <= 100
+
+
+import math
+import os
+import random
+import re
+import sys
+
+def staircase(n):
+    for i in range(1, n+1):
+        print(' '*(n-i) + '#'*i)
+        
+if __name__ == '__main__':
+    n = int(input())
+    staircase(n)
