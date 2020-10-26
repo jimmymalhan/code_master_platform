dde6ed5686c676e722f831e1524e7edb826e86e2
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-25 19:40:43 -0700

    commit

--- hacker_rank/Algorithms/001_Warm_up/010_time_conversions.py
+++ hacker_rank/Algorithms/001_Warm_up/010_time_conversions.py
@@ -1,5 +1,7 @@
-from time import strptime, strftime
+# Given a time 12- hour AM/PM format, convert it to military
+# (24hour) time
 
+from time import strptime, strftime
 def timeConversion(s):
     print(strftime("%H:%M:%S", strptime(s, "%I:%M:%S%p")))
 if __name__ == '__main__':
