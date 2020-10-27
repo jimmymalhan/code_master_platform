fbe64fae5c6e07fc860d07abbf2536180f91ec5d
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-27 04:10:10 -0700

    commit

--- hacker_rank/B.Practice/002_Basic Data Types/005_tuples.py
+++ hacker_rank/B.Practice/002_Basic Data Types/005_tuples.py
@@ -1,8 +1,6 @@
 # Given, a space 'n' integers as input
 # create a tuple 't' of those 'n' integers
 # compute and print the result of hash(t)
-# Note: hash() is one of the functions in __builtins__
-# module, so it needs to be imported
 
 # input format
 # first line contains an integer 'n', denoting the
@@ -17,5 +15,4 @@ n = int(input())
 integer_list = input().split()
 for i in range(len(integer_list)):
     integer_list[i] = int(integer_list[i])
-t = tuple(integer_list)
-print(hash(t))
+print(hash(tuple(integer_list)))
