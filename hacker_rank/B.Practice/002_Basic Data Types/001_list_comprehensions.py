2f0b6a91e8b70632bdc604ac05a560d315e266e7
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-27 06:31:53 -0700

    commit

---
+++ hacker_rank/B.Practice/002_Basic Data Types/001_list_comprehensions.py
@@ -0,0 +1,38 @@
+# Given, 3 integers x, y and z representing the dimensions of a 
+# cuboid 'n'. Print a list of all possible
+# coordinates given by (i,j,k) on a 3D grid where the sum of i + j + k
+# is not equal to 'n'.
+# NOTE : Use list comprehension instead of multiple loops
+
+# 0 <=i <=x
+# 0<=j<=y
+# 0 <=k <=z
+
+# Sample Input 0
+
+# 1
+# 1
+# 1
+# 2
+
+# Sample Output 0
+
+# [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
+
+x, y, z, n = [int(input()) for _ in range(4)]
+listofAnswers = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
+print(listofAnswers)
+
+# Explanation:
+# x = int(input())
+# y = int(input())
+# z = int(input())
+# n = int(input())
+# list = []
+
+# for i in range(0, x + 1):
+#     for j in range(0, y + 1):
+#             for z in range(0, z + 1):
+#                 if i + j + k != n:
+#                     list.append([i, j, k])
+# print(list)
