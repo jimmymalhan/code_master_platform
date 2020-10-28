7778b758b4b4f6e423c0274394718f24d2d9bb6f
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-28 06:38:40 -0700

    commit

--- hacker_rank/B.Practice/002_Basic Data Types/001_list_comprehensions.py
+++ hacker_rank/B.Practice/002_Basic Data Types/001_list_comprehensions.py
@@ -20,8 +20,7 @@
 # [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
 
 x, y, z, n = [int(input()) for _ in range(4)]
-listofAnswers = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
-print(listofAnswers)
+print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k !=n])
 
 # Explanation:
 # x = int(input())
