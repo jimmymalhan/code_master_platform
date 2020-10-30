a227553bafdffcf296859601a6a22b516a719e70
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-10-30 06:38:23 -0700

    commit

--- hacker_rank/A.30 Days of Code/020_sorting.py
+++ hacker_rank/A.30 Days of Code/020_sorting.py
@@ -1,5 +1,3 @@
-import sys
-
 n = int(input().strip())
 a = list(map(int, input().strip().split(' ')))
 # track number of elements swapped during a single array transversal
@@ -9,8 +7,8 @@ totalNumberOfSwaps = 0
 for i in range(n):
     currentSwaps = 0
 
-    for j in range(0, n-1):
-        if a[j] > a[j+1]:
+    for j in range(0, n - 1):
+        if a[j] > a[j + 1]:
             a[j], a[j + 1] = a[j + 1], a[j] # swap adjacent elements if they are in decreasing order
             currentSwaps += 1
             totalNumberOfSwaps += 1
@@ -20,4 +18,4 @@ for i in range(n):
 
 print('Array is sorted in ' + str(totalNumberOfSwaps) + ' swaps.')
 print('First Element: '+ str(a[0]))
-print('Last Element: ' + str(a[n-1]) )
+print('Last Element: ' + str(a[n - 1]))
