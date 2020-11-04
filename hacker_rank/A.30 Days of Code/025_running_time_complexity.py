11f745609f92c76530aa736ab72a54a3a347ade0
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-04 06:53:44 -0800

    commit

---
+++ hacker_rank/A.30 Days of Code/025_running_time_complexity.py
@@ -0,0 +1,31 @@
+#Brute Force
+# def isPrime(n):
+#     if n <= 1:
+#         return False
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+#     return True
+
+import math
+
+def isPrime(n):
+    if n <= 1:
+        return False
+    sqrt_n = math.sqrt(n)
+    if sqrt_n.is_integer():
+        return False
+    for i in range(2, int(sqrt_n) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+num_test_cases = int(input())
+for i in range(num_test_cases):
+    n = int(input())
+    if isPrime(n):
+        print('Prime')
+    else:
+        print('Not prime')
+
+    
