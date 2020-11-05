8e4e054224cdfbbffdd2ef3adafa1e99beaf309b
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-05 05:27:59 -0800

    commit

---
+++ hacker_rank/A.30 Days of Code/026_Nested_Logic.py
@@ -0,0 +1,20 @@
+return_day, return_month, return_year = [int(e) for e in input().strip().split(' ')] # creating a list is easier to do as list comprehension
+due_day, due_month, due_year = [int(e) for e in input().strip().split(' ')]
+
+#check the biggest category first: year
+if return_year < due_year:
+    print(0)
+elif return_year == due_year:
+    # check next biggest category: month
+    if return_month < due_month:
+        print(0)
+    elif return_month == due_month:
+        # check the last category: day
+        if return_day <= due_day:
+            print(0)
+        else:
+            print(15 * (return_day - due_day))
+    else:
+        print(500 * (return_month - due_month))
+else:
+    print(10000)
