b4bf87b70f5c882f7a736626de3e7e6cfa102492
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-10 06:15:48 -0800

    commit

---
+++ elements_of_programming/Ch - 5 Arrays Que/5.0.py
@@ -0,0 +1,6 @@
+# Ques - Array - Reorder entries that even entries appears first
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Ques - Covert a 2D list to 1D list as list comprehension
+M = [['a', 'b', 'c'], ['d', 'e', 'f']]
+[print(x) for row in M for x in row]
---
+++ elements_of_programming/Ch - 5 Arrays Que/5.1.py
@@ -0,0 +1,5 @@
+# Dutch National Flag Problem - 
+# Write a program that takes an array A and an index i into A, rearranges the elements such that all elements less than A[i] (the 'pivot') appears first,
+# followed by elements equal to the pivot, followed by elements greater than the pivot.
+
+# Hint - Think about partion step in quick sort
