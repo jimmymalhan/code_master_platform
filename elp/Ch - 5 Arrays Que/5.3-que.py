72ab7daecda7789fe965ef1f7c17f3ebe0a4e16d
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-12 06:16:25 -0800

    commit

---
+++ elp/Ch - 5 Arrays Que/5.3-que.py
@@ -0,0 +1,7 @@
+# Multiply two aribitary precision integers
+
+# Write a program that takes two arrays representing integers, and returns an integer representing their product.
+# For example, since 193707721 x -761838257287 = -127573952589676412927, if the inputs are {1,9,3,7,0,7,7,2,1} and
+# {-7,6,1,8,3,8,2,5,7,2,8,7}, your function should return {-1,4,7,5,7,3,9,5,2,5,8,9,6,7,6,4,1,2,9,2,7}
+
+# Hint: Use arrays to simulate the grade-school multuplication algorithm  
