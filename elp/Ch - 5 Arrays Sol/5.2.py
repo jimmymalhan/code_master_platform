5d2836d3ca8dc97d377b29b66e7236a71c7442b5
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-12 05:45:48 -0800

    commit

--- elp/Ch - 5 Arrays Sol/5.2.py
+++ elp/Ch - 5 Arrays Sol/5.2.py
@@ -3,4 +3,25 @@
 # you should update the array to [1, 3, 0]. Your algorithm should work even if its implemented in a
 # language that has finite - precision arithemetic. 
 
-# Hint:  Experiment with concrete examples
+# Hint:  Experiment with concrete examples # carry out # D + 1 
+
+A = [1,2,9]
+
+def plus_one(A):
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    if A[0] == 10:
+        # There is a carry-out, so we need one more digit to store the result.
+        # A slick way to do this is to append to 0 at the end of the array, 
+        # and update the first entry to 1.
+        A[0] = 1
+        A.append(0)
+    return A
+
+print(plus_one(A))
+# Time complexity = O(n)
+
