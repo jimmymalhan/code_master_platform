26c601985b0aed96b8c4e643350521596be2bcf4
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-14 05:19:33 -0800

    commit

--- elp/Ch - 5 Arrays Que/5.1_que.py
+++ elp/Ch - 5 Arrays Que/5.1_que.py
@@ -1,5 +1,6 @@
 # Dutch National Flag Problem
 
-# Write a program that takes an array A and an index i into A, rearranges the elements such that all elements less than A[i] (the 'pivot') appears first,
+# Write a program that takes an array A and an index i into A, rearranges the elements 
+# such that all elements less than A[i] (the 'pivot') appears first,
 # followed by elements equal to the pivot, followed by elements greater than the pivot.
 
