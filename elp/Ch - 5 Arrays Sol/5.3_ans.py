44cae1e8ace94469b28d5eadf2919d6c2dcaa024
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-15 06:21:59 -0800

    commit

--- elp/Ch - 5 Arrays Sol/5.3_ans.py
+++ elp/Ch - 5 Arrays Sol/5.3_ans.py
@@ -10,7 +10,7 @@ def multiply(num1, num2):
     num1[0], num2[0] = abs(num1[0]), abs (num2[0])
 
     result = [0] * (len(num1)+ len(num2))
-    for i in reversed(range(len(num1):
+    for i in reversed(range(len(num1))):
         for j in reversed(range(len(num2))):
             result[i + j + 1] += num1[i] * num2[j]
             result[i + j] += result[i + j + 1] // 10
