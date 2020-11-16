ba990e9e571b88ad06732ccc3e67c167991048e5
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-16 06:26:24 -0800

    commit

--- leet_code/Array/55.jump_game.py
+++ leet_code/Array/55.jump_game.py
@@ -2,7 +2,6 @@
 class Solution(object):
     def canJump(self, nums):
         last_position = len(nums) -1
-        
         for i in range(len(nums)-2,-1,-1):
             if (i + nums[i]) >= last_position:
                 last_position = i
