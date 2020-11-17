a5e947f2920472355350d7b76e9787f3cf0f2bb8
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-17 05:21:14 -0800

    commit

---
+++ elp/Ch - 5 Arrays Que/5.5- que.py
@@ -0,0 +1,5 @@
+# Write a program which takes as input a sorted array and updates it so that all duplicates have been removed and 
+# the remaining elements have been shifted left to fill the emptied indices. Return the number of valid elements.
+# Many languages have library functions for performing this operation you cannot use these functions.  
+
+A = [2,3,5,5,7,11,11,11,13]
