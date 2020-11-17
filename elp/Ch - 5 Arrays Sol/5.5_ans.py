a98c9e88fed62eae4a912087c79dd62575fc70e1
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-17 05:21:14 -0800

    commit

---
+++ elp/Ch - 5 Arrays Sol/5.5_ans.py
@@ -0,0 +1,20 @@
+# Delete duplicates from a sorted array
+
+# Write a program which takes as input a sorted array and updates it so that all duplicates have been removed and 
+# the remaining elements have been shifted left to fill the emptied indices. Return the number of valid elements.
+# Many languages have library functions for performing this operation you cannot use these functions.
+
+# Hint: There is an O(n)time and O(1) space solution
+
+# returns the number of valid enteries after deletion.
+
+def delete_duplicates(A):
+    if not A:
+        return 0
+
+    write_index = 1
+    for i in range(1, len(A)):
+        if A[write_index - 1] != A[i]:
+            A[write_index] = A[i]
+            write_index += 1
+    return write_index
