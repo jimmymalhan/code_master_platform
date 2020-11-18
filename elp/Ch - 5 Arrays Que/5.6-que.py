478a036cc7ce988f569b8813e4879082af9f3b6d
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-18 07:40:07 -0800

    commit

---
+++ elp/Ch - 5 Arrays Que/5.6-que.py
@@ -0,0 +1,5 @@
+# Write a program that takes an aray denoting daily stock price,
+# and returns the maximum profit that could be made by buying and
+# then selling one share of that stock. There is no need to buy if 
+# no profit is possible. 
+
---
+++ elp/Ch - 5 Arrays Sol/5.6_ans.py
@@ -0,0 +1,27 @@
+# Write a program that takes an aray denoting daily stock price,
+# and returns the maximum profit that could be made by buying and
+# then selling one share of that stock. There is no need to buy if 
+# no profit is possible. 
+
+# Hint: Identifying the minimum and maximum is not enough since the
+# minimum may appear after the maximum height. Focus on valid differences.
+
+# array of the minimum values seen so far 
+# maximum profit that can be made by selling on each specific day
+# is the difference of the current price and minimum seen so far. 
+# i.e [0,5,0,20,0,10,30,0,25,20]. hence, 30 is the maximum profit.
+
+def buy_and_sell_stock_once(prices):
+    min_price_so_far, max_profit = float('inf'), 0.0
+    for price in prices:
+        max_profit_sell_today = price - min_price_so_far
+        max_profit = max(max_profit, max_profit_sell_today)
+        min_price_so_far = min(min_price_so_far, price)
+    return max_profit_sell_today
+
+# time complexity - O(n)
+# space complexity - O(1)
+
+# max method takes two or more argument or at iterable,
+# For 2 arguments i.e max_profit and max_profit_sell_today,
+# it returns the variable which would have maximum value
