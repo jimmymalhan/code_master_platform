00fec2ef55efdca0476c586d23fc98685d37a480
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-20 03:34:41 -0800

    commit

--- 1.elp/Ch - 5 Arrays Que/5.4_que.py
+++ 1.elp/Ch - 5 Arrays Que/5.4_que.py
@@ -2,3 +2,5 @@
 # where A[i] denotes the maximum you can advance from
 # index i, and returns whether it is possible to advance
 # to the last index starting from the begining of the array
+
+# LC = 0055_jump_game
--- 1.elp/Ch - 5 Arrays Que/5.5_que.py
+++ 1.elp/Ch - 5 Arrays Que/5.5_que.py
@@ -2,4 +2,6 @@
 # the remaining elements have been shifted left to fill the emptied indices. Return the number of valid elements.
 # Many languages have library functions for performing this operation you cannot use these functions.  
 
+# LC = 0026 Remove Duplicates from Sorted Array
+
 A = [2,3,5,5,7,11,11,11,13]
