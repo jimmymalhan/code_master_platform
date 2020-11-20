bba8f99ac1c172e7d1fbd7d3288047eb5fdc8456
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-20 05:38:34 -0800

    commit

---
+++ 1.elp/Ch - 5 Arrays Que/5.8_que.py
@@ -0,0 +1,4 @@
+# Computing an Alternation
+
+# Write a program that takes an array A of n numbers, and rearranges A's elements to get a new array B having the property that
+# B[0] <= B[1] >= B[2] <= B[3] >= B[4] <= B[5] >= ... 
