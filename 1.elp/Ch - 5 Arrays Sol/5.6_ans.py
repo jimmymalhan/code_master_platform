52a24f91c347e576f63fc1cb186c4013a9c8dbc4
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-20 03:46:34 -0800

    commit

--- 1.elp/Ch - 5 Arrays Sol/5.6_ans.py
+++ 1.elp/Ch - 5 Arrays Sol/5.6_ans.py
@@ -13,10 +13,9 @@
 def buy_and_sell_stock_once(prices):
     min_price_so_far, max_profit = float('inf'), 0.0
     for price in prices:
-        max_profit_sell_today = price - min_price_so_far
-        max_profit = max(max_profit, max_profit_sell_today)
+        max_profit = max(max_profit, price - min_price_so_far)
         min_price_so_far = min(min_price_so_far, price)
-    return max_profit_sell_today
+    return max_profit
 
 # time complexity - O(n)
 # space complexity - O(1)
