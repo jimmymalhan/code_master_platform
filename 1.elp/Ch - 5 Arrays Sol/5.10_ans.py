53aca3b6b3c175e37cbd4228dec85fd66c92bce3
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-23 14:36:06 -0800

    commit

---
+++ 1.elp/Ch - 5 Arrays Sol/5.10_ans.py
@@ -0,0 +1,4 @@
+# Permute the elements of an array
+
+# Given an array A of n elements and permutation P, apply P to A.
+
--- 1.elp/Ch - 5 Arrays Sol/5.9_ans.py
+++ 1.elp/Ch - 5 Arrays Sol/5.9_ans.py
@@ -15,7 +15,7 @@ def generate_primes(n):
     for p in range(2, n + 1):
         if is_prime[p]:
             primes.append(p)
-            #Sieve p's multuples.
+            #Sieve p's multiples.
             for i in range(p , n+1 , p):
                 is_prime[i] = False
     return primes
@@ -45,3 +45,38 @@ def generate_primes(n):
             for j in range(2 * i**2 + 6 * i +3, size, p):
                 is_prime[j] = False
     return primes
+
+# Python program to print all primes smaller than or equal to 
+# n using Sieve of Eratosthenes 
+
+# https://www.geeksforgeeks.org/sieve-of-eratosthenes/ 
+def SieveOfEratosthenes(n): 
+      
+    # Create a boolean array "prime[0..n]" and initialize 
+    #  all entries it as true. A value in prime[i] will 
+    # finally be false if i is Not a prime, else true. 
+    prime = [True for i in range(n+1)] 
+    p = 2
+    while (p * p <= n): 
+          
+        # If prime[p] is not changed, then it is a prime 
+        if (prime[p] == True): 
+              
+            # Update all multiples of p 
+            for i in range(p * p, n+1, p): 
+                prime[i] = False
+        p += 1
+      
+    # Print all prime numbers 
+    for p in range(2, n+1): 
+        if prime[p]: 
+            print (p)
+  
+# driver program 
+if __name__=='__main__': 
+    n = 30
+    print ("Following are the prime numbers smaller",) 
+    print ("than or equal to", n)
+    print(SieveOfEratosthenes(n))
+
+Time complexity : O(n*log(log(n)))
