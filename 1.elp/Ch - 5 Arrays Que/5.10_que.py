0d4db9653bc627a9f5a70c7d786fc6c082e89846
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-24 09:43:29 -0800

    commit

--- 1.elp/Ch - 5 Arrays Que/5.10_que.py
+++ 1.elp/Ch - 5 Arrays Que/5.10_que.py
@@ -1,1 +1,9 @@
 # Given an array A of n elements and permutation P, apply P to A.
+
+def apply_permutation(A,P):
+
+
+if __name__ == '__main__': 
+    A = [8, 7, 6, 5] 
+    P = [3, 2, 1, 0] 
+    print(apply_permutation(A, P))
