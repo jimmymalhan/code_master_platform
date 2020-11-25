41c7cc6e2d95800b66fad85d058ee6fac3e582f5
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-25 05:21:26 -0800

    commit

--- 1.elp/Ch - 5 Arrays Que/5.9_que_prime_numbers.py
+++ 1.elp/Ch - 5 Arrays Que/5.9_que_prime_numbers.py
@@ -1,3 +1,12 @@
 # Write a program that takes an integer argument and returns all the primes between 1 and that 
 # integer. For example, if the input is 18, you should return
 # [2,3,5,6,7,11,13,17]
+
+def SieveofEratosthenes(n):
+
+
+if __name__=='__main__': 
+    n = 30
+    print ("Following are the prime numbers smaller") 
+    print ("than or equal to", n)
+    print(SieveOfEratosthenes(n))
