21e1387b76624bf37055419b4501f8c58d0b56df
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-11-29 06:30:05 -0800

    commit

--- 1.elp/Ch - 5 Arrays Que/5.0_que.py
+++ 1.elp/Ch - 5 Arrays Que/5.0_que.py
@@ -2,10 +2,11 @@
 # You are required to solve it without allocating additional storage
 # Also, add space and time complexity
 
+# Write pseudo code here
+
 def even_odd(A):
     
 
-
 if __name__ == "__main__":
-    print(even_odd(A))
     A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(even_odd(A))
