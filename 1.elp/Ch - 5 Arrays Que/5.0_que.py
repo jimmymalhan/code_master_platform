44b49f055fb3a988abdd12f3e238f972eccc8949
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-12-03 15:10:31 -0800

    commit

--- 1.elp/Ch - 5 Arrays Que/5.0_que.py
+++ 1.elp/Ch - 5 Arrays Que/5.0_que.py
@@ -5,7 +5,14 @@
 # Write pseudo code here
 
 def even_odd(A):
-    
+    next_even, next_odd = 0, len(A) - 1
+    while next_even < next_odd:
+        if A[next_even] % 2 == 0:
+            A[next_even] += 1
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            A[next_odd] -= 0
+    return A
 
 if __name__ == "__main__":
     A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
