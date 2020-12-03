6b5657579a986d0990e2794ea2a702935b4a07ad
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-12-03 15:27:44 -0800

    commit

--- 3.algoexpert/Easy/insertion_sort.py
+++ 3.algoexpert/Easy/insertion_sort.py
@@ -1,13 +1,12 @@
-def insetion_sort(array):
-    for i in range(1, len(array)):
-        j = i # insertion
-        while j > 0 and array[j] < array[j - 1]: # indices
-            swap(j, j-1, array)
-            j -= 1
-    return array
-
+def insertionSort(array):
+	for i in range(1, len(array)):
+		j = i # insertion
+		while j > 0 and array[j] < array[j-1]: # swapping indices
+			swap(j, j-1, array)
+			j -= 1
+	return array
 def swap(i, j, array):
-    array[i], array[j] = array[j], array[i]
+	array[i], array[j] = array[j], array[i]
 
 if __name__ == "__main__":
     array = [9,8,4,2,5,3]
