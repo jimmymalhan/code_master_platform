c469c44fc96c5741f010d951f029a7fd44acdaf3
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-12-12 08:52:06 -0800

    commit

---
+++ 3.algoexpert/2.Medium/min_heap_construction.py
@@ -0,0 +1,52 @@
+# Do not edit the class below except for the buildHeap,
+# siftDown, siftUp, peek, remove, and insert methods.
+# Feel free to add new properties and methods to the class.
+class MinHeap:
+    def __init__(self, array):
+        self.heap = self.buildHeap(array)
+
+	# O(n) time | O(1) space
+    def buildHeap(self, array):
+		firstParentIdx = (len(array) - 2) // 2
+		for currentIdx in reversed(range(firstParentIdx + 1)):
+			self.siftDown(currentIdx, len(array) - 1, array)
+		return array
+	
+	# O(log(n) time, O(1) space)
+    def siftDown(self, currentIdx, endIdx, heap):
+		childOneIdx = currentIdx * 2 + 1 # formula
+		while childOneIdx <= endIdx:
+			childTwoIdx = currentIdx * 2 + 2 if currentIdx * 2 + 2 <= endIdx else -1
+			if childTwoIdx != -1 and heap[childTwoIdx] < heap[childOneIdx]:
+				idxToSwap = childTwoIdx
+			else:
+				idxToSwap = childOneIdx
+			if heap[idxToSwap] < heap[currentIdx]:
+				self.swap(currentIdx, idxToSwap, heap)
+				currentIdx = idxToSwap
+				childOneIdx = currentIdx * 2 + 1
+			else:
+				return	
+
+    def siftUp(self, currentIdx, heap):
+		parentIdx = (currentIdx - 1) // 2 #formula to check
+		while currentIdx > 0 and heap[currentIdx] < heap[parentIdx]:
+			self.swap(currentIdx, parentIdx, heap)
+			currentIdx = parentIdx
+			parentIdx = (currentIdx - 1) // 2
+			
+    def peek(self):
+		return self.heap[0] # peeking root node
+
+    def remove(self):
+		self.swap(0, len(self.heap) - 1, self.heap)#swapping the two value
+		valueToRemove = self.heap.pop()
+		self.siftDown(0, len(self.heap) -1, self.heap)
+		return valueToRemove
+		
+    def insert(self, value):
+		self.heap.append(value) # add the value 
+		self.siftUp(len(self.heap) - 1, self.heap) # siftup = two values - current index & heap
+		
+	def swap(self, i, j, heap):
+		heap[i], heap[j] = heap[j], heap[i]
---
+++ 3.algoexpert/test.py
@@ -0,0 +1,4 @@
+array = [1, 2, 3, 4]
+
+for i in range(len(array)):
+    print(array[i]+array[i+1], end=' ')
