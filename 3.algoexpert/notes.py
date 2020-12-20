63920bb785c40a86cd7942f1dad75b56d4022b1c
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-12-20 12:20:59 -0800

    commit

--- 3.algoexpert/notes.py
+++
@@ -1,4 +0,0 @@
-array = [1, 2, 3, 4]
-
-for i in range(len(array)):
-    print(array[i]+array[i+1], end=' ')
---
+++ 3.algoexpert/test.py
@@ -0,0 +1,9 @@
+array = [1, 2, 3, 4]
+
+print(array)
+## for i in range(len(array) - 1):
+# for i in range(len(array)): # index out out bound
+#     print(array[i]+array[i+1], end=' ')
+
+for i in range(len(array)):
+    print(array[i])
