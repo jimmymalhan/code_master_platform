19937b4c70bc203db02ef6e81febdf4653a16798
Jimmy Malhan <jimmymalhan999@gmail.com>
2020-12-27 08:24:30 -0800

    commit

--- 3.algoexpert/2.Medium/1.bst_construction.py
+++ 3.algoexpert/2.Medium/1.bst_construction.py
@@ -7,11 +7,11 @@ class BST:
 	# Average: O(log(n)) time | O(1) space
 	# Worst : O(n) time | O(1) space
     def insert(self, value):
-		currentNode = self # intialize the current node and tells you where you are with the currentNode
+		currentNode = self # intialize the currentNode and tells you're with the currentNode
 		while True:
 			if value < currentNode.value: # traverse left subtree
 				if currentNode.left is None: 
-					currentNode.left = BST(value) # #currentNode.left = assigning it to left subtree | successfully inserted the value
+					currentNode.left = BST(value) # currentNode.left = assigning it to left subtree # successfully inserted the value
 					break
 				else: # currentNode.left is not None
 					currentNode = currentNode.left # still have left subtree to explore
@@ -32,22 +32,22 @@ class BST:
 				currentNode = currentNode.left
 			elif value > currentNode.value: # or if value is greater than currentNode's value, explore right subtree
 				currentNode = currentNode.right
-			else: # or value == currentNode.value # found the value
+			else: # or value = currentNode.value # found the value
 				return True
-		return False # if currentNode is None or null |  value didn't match
+		return False # value didn't match # if currentNode is None or Null
 
     def remove(self, value, parentNode = None):
     	currentNode = self 
 		while currentNode is not None:  # removal is a two step process 1) find the node 2) remove the node
 			if value < currentNode.value: 
-				parentNode = currentNode # keep track of parent/root node as to reassign the childNode to the parent of the node that we are removing
+				parentNode = currentNode # keep track of parent/root node as to re-assign the childNode to the parent of the node that we are removing
 				currentNode = currentNode.left
 			elif value > currentNode.value:
 				parentNode = currentNode
 				currentNode = currentNode.right
 			else: # value = currentNode and found the value | Deals with 2 subcases
 				if currentNode.left is not None and currentNode.right is not None: # FIRST SUBCASE - ROOT HAS 2 CHILD NODES
-					currentNode.value = currentNode.right.getMinValue() # currentNode.value = smallest value of right subtree # get the smallest value in the given right subtree 
+					currentNode.value = currentNode.right.getMinValue() # currentNode.value = smallest value of right subtree
 					currentNode.right.remove(currentNode.value, currentNode) # currentNode is passing as parentNode
 				elif parentNode is None: # SECOND SUBCASE - ROOT DOESN't HAVE 2 CHILDREN NODES AND ROOT node doesn't have a parentNode
 					# will get back to it (TRICKY)
@@ -61,7 +61,7 @@ class BST:
 						currentNode.right = currentNode.right.right # important to assign the right value in the last line so as to overwrite
 					else: # root node which doesn't have a parent node and root node which doesn't have children nodes # This is a single-node tree; do nothing
 						pass # currentNode.value = None # can discuss with the interviewer for this used case scenario
-				elif parentNode.left == currentNode: # ONLY have 1 childNode or none and if the node is left child or right child
+				elif parentNode.left == currentNode: # ONLY have 1 childNode or none, if the node is left child or right child
 					parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right # left child of the parent node
 				elif parentNode.right == currentNode:
 					parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right # right child of the parent node
