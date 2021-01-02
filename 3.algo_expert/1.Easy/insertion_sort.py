8660e8ab98c9a56cc1cfdfd8188f4770fce5b562
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-01-02 06:57:33 -0800

    commit

--- 3.algo_expert/1.Easy/insertion_sort.py
+++ 3.algo_expert/1.Easy/insertion_sort.py
@@ -1,9 +1,13 @@
+# O(n^2) time | O(1) space
 def insertionSort(array):
-	for i in range(1, len(array)):
-		j = i # insertion
-		while j > 0 and array[j] < array[j-1]: # swapping indices
+	# Looping through the array once
+	for i in range(1, len(array)): # intialize the 'for' loop # keep inserting numbers to the tentative sorted sublist(very first number of the array)
+		j = i # 2nd index
+		# looping backwards through the array and doing bunch of swaps to poisition, our current number at current place in the tentatively sorted sublist
+		while j > 0 and array[j] < array[j - 1]: # j = current number > 0 (haven't reached the very begining of the array | tentatively sorted sublist)
+			# AND while our current number(array[j]) is out of position, less than a number before it (array[j - 1])
 			swap(j, j-1, array)
-			j -= 1
+			j -= 1 # decrement j and keep track of the number that we are trying to insert
 	return array
 def swap(i, j, array):
 	array[i], array[j] = array[j], array[i]
