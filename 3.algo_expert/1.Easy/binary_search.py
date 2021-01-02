633af2673005ec13c654a991881be0779928b34d
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-01-02 10:02:11 -0800

    commit

--- 3.algo_expert/1.Easy/binary_search.py
+++ 3.algo_expert/1.Easy/binary_search.py
@@ -1,3 +1,20 @@
+# O(log(n)) time | O(log(n)) space
+def binarySearch(array, target):
+	return binarySearchHelper(array, target, 0, len(array) - 1)
+
+def binarySearchHelper(array, target, left, right):
+	if left > right:
+        return - 1 # if number is not found return -1
+    middle = (left + right) // 2
+    potentialMatch = array[middle]
+    if target == potentialMatch:
+        return middle
+    elif target < potentialMatch:
+	    return binarySearchHelper(array, target, left, middle - 1) # middle -1 = right pointer # eliminating the entire right half of an array, move right pointer one position to the left of the middle pointer
+    else: # target > potentialMatch
+	    return binarySearchHelper(array, target, middle + 1, right) # eliminate the left half of an array, move left pointer one position to the right of the middle pointer
+
+# O(log(n)) time | O(1) space
 def binarySearch(array, target):
 	return binarySearchHelper(array, target, 0, len(array) - 1)
 
