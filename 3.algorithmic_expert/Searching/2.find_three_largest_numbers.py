0a45fc36100b71cd5e67774ac1ed3fb67dc2878b
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-01-03 17:50:57 -0800

    commit

--- 3.algorithmic_expert/Searching/2.find_three_largest_numbers.py
+++ 3.algorithmic_expert/Searching/2.find_three_largest_numbers.py
@@ -1,21 +1,21 @@
 # O(n) time | O(1) space
 def findThreeLargestNumbers(array):
-	threeLargest = [None, None, None]
-	for num in array: # traversing the array
+	threeLargest = [None, None, None] # Intializing the array
+	for num in array: #num is the currentvalue # traversing the array
 		updateLargest(threeLargest, num)
 	return threeLargest
 	
 def updateLargest(threeLargest, num):
-	if threeLargest[2] is None or num > threeLargest[2]:
-		shiftAndUpdate(threeLargest, num, 2)
+	if threeLargest[2] is None or num > threeLargest[2]: # compare the currentNumber "num" to the 3rd Value in the 'threeLargest' array represents the largest number
+		shiftAndUpdate(threeLargest, num, 2) # shifting numbers to the left in the array
 	elif threeLargest[1] is None or num > threeLargest[1]:
 		shiftAndUpdate(threeLargest, num, 1)
 	elif threeLargest[0] is None or num > threeLargest[0]:
 		shiftAndUpdate(threeLargest, num, 0)
 		
 def shiftAndUpdate(array, num, idx):
-	for i in range(idx + 1):
-		if i == idx:
-			array[i] = num
+	for i in range(idx + 1): #idx + 1 is exclusive # 0 to idx
+		if i == idx:# At the point of iteration where, we are at last index that have passed in the method
+			array[i] = num # update the number to the 'threelargest' array
 		else:
-			array[i] = array[i + 1]
+			array[i] = array[i + 1] #
