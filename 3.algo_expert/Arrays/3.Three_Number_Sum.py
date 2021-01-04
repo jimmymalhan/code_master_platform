c36a42d596fd5c43b5a1adcabaaf49679341367c
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-01-03 17:12:03 -0800

    commit

--- 3.algo_expert/Arrays/3.Three_Number_Sum.py
+++ 3.algo_expert/Arrays/3.Three_Number_Sum.py
@@ -1,11 +1,11 @@
 def threeNumberSum(array, targetSum):
 	array.sort()
 	triplets = []
-	for i in range(len(array) - 2):
+	for i in range(len(array) - 2):# array[i]/currentNumber is the first element of an array
 		left = i + 1
 		right = len(array) - 1
 		while left < right:
-			currentSum = array[i] + array[left] + array[right]
+			currentSum = array[i] + array[left] + array[right] #formula
 			if currentSum == targetSum:
 				triplets.append([array[i], array[left], array[right]])
 				left += 1
