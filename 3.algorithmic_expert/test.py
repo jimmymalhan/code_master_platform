6705737007f1863f6f91ba77041742592b7337d0
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-01-04 06:09:34 -0800

    commit

--- 3.algorithmic_expert/test.py
+++ 3.algorithmic_expert/test.py
@@ -1,12 +1,17 @@
 array = [1, 2, 3, 4]
 
-print(array)
-## for i in range(len(array) - 1):
-# for i in range(len(array)): # index out out bound
-#     print(array[i]+array[i+1], end=' ')
+# print(array) # [1, 2, 3, 4]
+# for i in range(len(array) - 1):
+# # for i in range(len(array)): # index out out bound
+#     print(array[i]+array[i+1], end=' ') #3, 5, 7
 
-for i in range(len(array)):
-    print(array[i])
+# for i in range(len(array)):
+#     print(array[i]) # 1, 2, 3, 4
+#     # print(i) # 0, 1, 2, 3, 4
 
 #  = equal
 #  == equalized
+
+def range1(start, end):
+    return range(start, end+1)
+print(range1(1,10))
