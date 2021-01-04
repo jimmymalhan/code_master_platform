8a2f31d9c6600bdc6163880b57d7b209bbc3cfa9
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-01-03 17:12:03 -0800

    commit

--- 3.algo_expert/Arrays/1.two_number_sum.py
+++ 3.algo_expert/Arrays/1.two_number_sum.py
@@ -1,21 +1,21 @@
-# O(n^2) time | O(1) space
-def twoNumberSum(array, targetSum):
-	for i in range(len(array) - 1):
-		for j in range(i + 1, len(array)):
-			if array[i] + array[j] == targetSum:
-				return [array[i], array[j]]
-	return[]
+# # O(n^2) time | O(1) space
+# def twoNumberSum(array, targetSum):
+# 	for i in range(len(array) - 1):
+# 		for j in range(i + 1, len(array)):
+# 			if array[i] + array[j] == targetSum:
+# 				return [array[i], array[j]]
+# 	return[]
 
-# O(n) time | O(1) space
-def twoNumberSum(array, targetSum):
-	nums = {}
-	for num in array:
-		potentialMatch = targetSum - num
-		if potentialMatch in nums:
-			return [potentialMatch, num]
-		else:
-			nums[num] = True
-	return[]
+# # O(n) time | O(1) space
+# def twoNumberSum(array, targetSum):
+# 	nums = {}
+# 	for num in array:
+# 		potentialMatch = targetSum - num
+# 		if potentialMatch in nums:
+# 			return [potentialMatch, num]
+# 		else:
+# 			nums[num] = True
+# 	return[]
 
 # Time O(nlog(n)) | O(1) space
 def twoNumberSum(array, targetSum):
