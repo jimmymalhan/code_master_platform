f13ff92ed1953ded1d5d595226b2a8e4b9f21513
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-01-06 06:35:35 -0800

    commit

---
+++ 3.algorithmic_expert/Binary Trees/branch_sums.py
@@ -0,0 +1,23 @@
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+# O(n) time | O(1) space - where n is the nodes in the Binary Tree
+def branchSums(root):
+	sums = []
+	calculateBranchSums(root, 0, sums)
+	return sums
+
+def calculateBranchSums(node, runningSum, sums):
+	if node is None:
+		return
+	
+	newRunningSum = runningSum + node.value
+	if node.left is None and node.right is None:
+		sums.append(newRunningSum)
+		return
+	
+	calculateBranchSums(node.left, newRunningSum, sums)
+	calculateBranchSums(node.right, newRunningSum, sums)
