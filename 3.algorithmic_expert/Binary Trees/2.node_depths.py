c693726a2c05f3d55deb0c7a90e473a34b83e3bf
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-01-12 06:06:11 -0800

    commit

--- 3.algorithmic_expert/Binary Trees/2.node_depths.py
+++ 3.algorithmic_expert/Binary Trees/2.node_depths.py
@@ -1,11 +1,12 @@
 """
-tree =              1
-                  /   \
-                2       3
-               / \     /  \
-              4   5   6    7
+tree =              1(0)
+                  /      \
+                2(1)       3(1)
+               / \          /  \
+           (2)4   5(2)  (2)6    7(2)
             /   \
-           8     9
+        (3)8     9(3)
+# in brackets are the depth of the nodes
 
 Sample Output - 16
 The depth of the node with value 2 is 1. # process of calculation DEPTH of the tree (DFS) begins from the ROOT of the node
@@ -18,31 +19,26 @@ The depth of the node with value 8 is 3.
 The depth of the node with value 9 is 3.
 Summing all of these depths yields 16.
 
-1
-3 2
-3 5 4
-3 4 9 8
-3 5 9
-3 5
-3
-
-recursive - f(n, d) = d + f(l, d +1) + f(r , d + 1)
-
+recursive - f(n, d) = d + f(l, d + 1) + f(r , d + 1)
+d = currentDepth /rootNode
+d + 1 = depth of the childNode(1 level deeper)
 n - node
 d - depth
 l - left
 r - right
 """
-"""
-recursive - f(n, d) = d + f(l, d + 1) + f(r , d + 1)
-n - node | d - depth | l - left | r - right
-"""
 # avg case: when tree is balanced
-# O(n) time | O(h) space where n is the number of nodes in the binary tree and h is the height of the binary tree
+# O(n) time - constantly adding the value of depth on each node
+# O(h) space where n is the number of nodes in the binary tree and h is the height of the binary tree -> keeps calling function
+# of the call stack from rootNode to get the depth of the childrenNode
+
+# if the tree is not balanced, space complexity will be O(log(n))
 def nodeDepths(root, depth=0):
 	if root is None: # base case - if node has null value
 		return 0
-	return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1) # adding depth to the runningSum
+	return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+  # depth -> the root node
+  # depth + 1 -> a level down to 
 
 class BinaryTree:
     def __init__(self, value):
@@ -51,8 +47,16 @@ class BinaryTree:
         self.right = None
 
 """Iterative
-empty stack -> grab root node (1) with the depth 0 and add it to the stack (to know it's depth with the runningNode) -> pop it off the node     # if the stack isn't empty, keep applying the algorithm
-grab child nodes (2) and (3) with the depth 1 and add it to the stack (to know it's depth with the runningNode) -> pop it off the node 
+empty stack -> grab root node (1) with the depth 0 and add it to the stack (to know it's depth with the runningNode) -> pop it off the node # if the stack isn't empty, keep applying the algorithm
+grab child nodes (2) and (3) with the depth 1 and add it to the stack (to know it's depth with the runningNode) -> pop it off the node
+
+1
+3 2
+3 5 4
+3 4 9 8
+3 5 9
+3 5
+3
 """
 
 # avg case: when tree is balanced
