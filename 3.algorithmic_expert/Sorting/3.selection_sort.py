a9c84af82c1f5cb841dd850bae90bda307359bbb
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-02-01 08:05:50 -0800

    commit

--- 3.algorithmic_expert/Sorting/3.selection_sort.py
+++ 3.algorithmic_expert/Sorting/3.selection_sort.py
@@ -5,15 +5,15 @@
 # Worst: O(n^2) time | O(1)space
 
 def selectionSort(array):
-    currentIdx = 0
-    while currentIdx < len(array) - 1:
-        smallestIdx = currentIdx
-        for i in range(currentIdx + 1, len(array)):
-            if array[smallestIdx] > array[i]:
-                smallestIdx = i
-        swap(currentIdx, smallestIdx, array)
-        currentIdx += 1
-    return array
+	currentIdx = 0
+	while currentIdx < len(array) - 1:
+		smallestIdx = currentIdx # value is 0
+		for i in range(currentIdx + 1, len(array)):
+			if array[smallestIdx] > array[i]: # if 0 > currentNumber- array[i]
+				smallestIdx = i # append smallestIdx to be i
+		swap(currentIdx, smallestIdx, array)
+		currentIdx += 1
+	return array
 
 def swap(i, j, array):
-    array[i], array[j] = array[j], array[i]
+	array[i], array[j] = array[j], array[i]
