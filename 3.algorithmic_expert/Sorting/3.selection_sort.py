c9296e203077da98f3a28cba8146f7d2296b228e
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-02-02 09:01:30 -0800

    commit

--- 3.algorithmic_expert/Sorting/3.selection_sort.py
+++ 3.algorithmic_expert/Sorting/3.selection_sort.py
@@ -7,10 +7,10 @@
 def selectionSort(array):
 	currentIdx = 0 #starting point
 	while currentIdx < len(array) - 1:
-		smallestIdx = currentIdx # value is 0
-		for i in range(currentIdx + 1, len(array)):
-			if array[smallestIdx] > array[i]: # if 0 > currentNumber(array[i])
-				smallestIdx = i # appending smallestIdx to be i
+		smallestIdx = currentIdx # (smallestidx is the first 1st idx) # value is 0
+		for i in range(currentIdx + 1, len(array)): # from 2nd idx to len(array)
+			if array[smallestIdx] > array[i]: # if 1st element(SMALLEST NUMBER) in the array > 2nd element to end of the array(CURRENT NUMBER)
+				smallestIdx = i # appending (idx) smallestIdx(0) to be i (currentIdx)
 		swap(currentIdx, smallestIdx, array)
 		currentIdx += 1
 	return array
