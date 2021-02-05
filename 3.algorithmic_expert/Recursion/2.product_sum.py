eb73151630d4661190e514d0b9c2265bddac90ee
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-02-05 10:57:53 -0800

    commit

--- 3.algorithmic_expert/Recursion/2.product_sum.py
+++ 3.algorithmic_expert/Recursion/2.product_sum.py
@@ -1,1 +1,11 @@
 # special array contains integers or other special arrays -> Recursion problem
+
+# O(n) time | O(d) space = d is depth
+def productSum(array, multipler = 1):
+	sum = 0
+	for element in array:
+		if type(element) is list:
+			sum += productSum(element, multipler + 1)
+		else:
+			sum += element
+	return sum * multipler
