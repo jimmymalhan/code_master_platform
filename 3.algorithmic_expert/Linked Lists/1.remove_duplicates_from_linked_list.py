00f70068af426b6f26decc8414bb97361b9d16d5
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-02-07 06:39:48 -0800

    commit

--- 3.algorithmic_expert/Linked Lists/1.remove_duplicates_from_linked_list.py
+++ 3.algorithmic_expert/Linked Lists/1.remove_duplicates_from_linked_list.py
@@ -3,15 +3,15 @@ class LinkedList:
         self.value = value
         self.next = None
 
-
+# O(n) time - one loop | O(1) space
 def removeDuplicatesFromLinkedList(linkedList):
-	currentNode = linkedList
+	currentNode = linkedList # headNode
 	while currentNode is not None:
 		nextDistinctNode = currentNode.next
-		while nextDistinctNode is not None and nextDistinctNode.value == currentNode.value:
-			nextDistinctNode = nextDistinctNode.next
+		while nextDistinctNode is not None and nextDistinctNode.value == currentNode.value: # when value of next node is equal to current node
+			nextDistinctNode = nextDistinctNode.next # setting the pointer to nextDistinct Node
 			
-		currentNode.next = nextDistinctNode
-		currentNode = nextDistinctNode
+		currentNode.next = nextDistinctNode # removing duplicates
+		currentNode = nextDistinctNode # changing the pointer to nextDistinct Node
+	
 	return linkedList
-		
