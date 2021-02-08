4d30cd8d5888d535b23c6f9cd8d12c438054f0e6
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-02-07 18:03:52 -0800

    commit

--- 3.algorithmic_expert/Linked Lists/1.remove_duplicates_from_linked_list.py
+++ 3.algorithmic_expert/Linked Lists/1.remove_duplicates_from_linked_list.py
@@ -9,7 +9,7 @@ def removeDuplicatesFromLinkedList(linkedList):
 	while currentNode is not None:
 		nextDistinctNode = currentNode.next
 		while nextDistinctNode is not None and nextDistinctNode.value == currentNode.value: # when value of next node is equal to current node
-			nextDistinctNode = nextDistinctNode.next # setting the pointer to nextDistinct Node
+			nextDistinctNode = nextDistinctNode.next # setting the pointer for nextDistinct Node
 			
 		currentNode.next = nextDistinctNode # removing duplicates
 		currentNode = nextDistinctNode # changing the pointer to nextDistinct Node
