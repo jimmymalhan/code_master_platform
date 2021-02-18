70ae9ab87e3b65a5e639a9735e2db4458f73e857
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-02-18 06:27:01 -0800

    updates

--- 3.algorithmic_expert/Sorting/5.quick_sort.py
+++ 3.algorithmic_expert/Sorting/5.quick_sort.py
@@ -5,6 +5,8 @@
 # start >= end
 # right >= left
 	# L > P and R < P
+	# swap
+# R < L # swap
 
 # Worst : time - O(n^2),Space- O(log(n)) # in input - swap all positions
 # Best: time - O(nlog(n)),Space- O(log(n)) # in input - swap postions for left and right subarray
@@ -28,15 +30,15 @@ def quickSortHelper(array, startIdx, endIdx):
 		if array[rightIdx] >= array[pivotIdx]:
 			rightIdx -= 1
 	swap(pivotIdx, rightIdx, array) # R < L
-	# leftSubarray = rightIdx - 1 ==>> 'ending index - endIdx' ( rightIdx - 1 - startIdx)
+	# leftSubarray = (rightIdx - 1) -> pivot is located here
 	# rightSubarray = endIdx - (rightIdx + 1)
 	leftSubarrayIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1)
 	if leftSubarrayIsSmaller:
-		quickSortHelper(array, startIdx, rightIdx - 1)
-		quickSortHelper(array, rightIdx + 1, endIdx)
+		quickSortHelper(array, startIdx, rightIdx - 1) # leftSubarray
+		quickSortHelper(array, rightIdx + 1, endIdx) # rightSubarray
 	else:
-		quickSortHelper(array, rightIdx + 1, endIdx)
-		quickSortHelper(array, startIdx, rightIdx - 1)
+		quickSortHelper(array, rightIdx + 1, endIdx) # rightSubarray
+		quickSortHelper(array, startIdx, rightIdx - 1) # leftSubarray
 	
 def swap(i, j, array):
 	array[i], array[j] = array[j], array[i]
