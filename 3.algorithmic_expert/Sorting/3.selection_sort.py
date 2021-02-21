9df38af63e0dacbfef4f37329d9113ee1970b47f
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-02-21 10:06:18 -0800

    updates

--- 3.algorithmic_expert/Sorting/3.selection_sort.py
+++ 3.algorithmic_expert/Sorting/3.selection_sort.py
@@ -1,15 +1,17 @@
-# Concept - create a smallest number(as idx), append it with the idx(bigger idx) and swap it (with the first element)
+# divide arr: sorted arr with len of 0, 
+# find smallest num in unsorted array, 
+# insert it into sorted subarray with swap
 
 # Best: O(n^2) time | O(1)space
 # Average: O(n^2) time | O(1)space
 # Worst: O(n^2) time | O(1)space
 
 def selectionSort(array):
-	currentIdx = 0 #starting point
-	while currentIdx < len(array) - 1:
-		smallestIdx = currentIdx # (smallestidx is the first 1st idx) # value is 0
+	currentIdx = 0 # sorted subarray with len 0
+	while currentIdx < len(array) - 1: # finding smallest num in unsorted subarray
+		smallestIdx = currentIdx # smallestidx is the 1st idx with value 0
 		for i in range(currentIdx + 1, len(array)): # from 2nd idx to len(array)
-			if array[smallestIdx] > array[i]: # if 1st element(SMALLEST NUMBER) in the array > 2nd element to end of the array(CURRENT NUMBER)
+			if array[smallestIdx] > array[i]: # if 1st idx(SMALLEST NUMBER) in the array > 2nd idx to end of the array(CURRENT NUMBER)
 				smallestIdx = i # appending (idx) smallestIdx(0) to be i (currentIdx)
 		swap(currentIdx, smallestIdx, array)
 		currentIdx += 1
