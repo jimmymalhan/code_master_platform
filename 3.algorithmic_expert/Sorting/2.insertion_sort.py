32f1761e269c6121fb68ab5a1beb2cff52dd3e43
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-02-22 08:36:07 -0800

    updates

--- 3.algorithmic_expert/Sorting/2.insertion_sort.py
+++ 3.algorithmic_expert/Sorting/2.insertion_sort.py
@@ -3,7 +3,7 @@ def insertionSort(array):
 	# Looping through the array once
 	for i in range(1, len(array)): # intialize the 'for' loop # keep inserting numbers to the tentative sorted sublist(very first number of the array)
 		j = i # 2nd index
-		# looping backwards through the array and doing bunch of swaps to poisition, our current number at current place in the tentatively sorted sublist
+		# looping backwards through the array and doing bunch of swaps to position, our current number at current place in the tentatively sorted sublist
 		while j > 0 and array[j] < array[j - 1]: # j = current number > 0 (haven't reached the very begining of the array | tentatively sorted sublist)
 			# AND while our current number(array[j]) is out of position, less than a number before it (array[j - 1])
 			swap(j, j-1, array)
