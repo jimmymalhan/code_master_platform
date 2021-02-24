5ed2cea120ed248671f1198e8a75dcca3fa6a485
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-02-24 09:01:47 -0800

    updates

--- 3.algorithmic_expert/Greedy Algorithm/1.minimum_waiting_time.py
+++ 3.algorithmic_expert/Greedy Algorithm/1.minimum_waiting_time.py
@@ -1,10 +1,11 @@
-# (nlogn) time | O(1) space
+# O(nlogn) time -> (because of sorting)| O(1) space
+# queries will always be 1 ?
 def minimumWaitingTime(queries):
-    queries.sort()
-	
+	queries.sort()
 	totalWaitingTime = 0
+	
 	for idx, duration in enumerate(queries):
-		queriesLeft = len(queries) - (idx + 1)
+		queriesLeft = len(queries) - (idx + 1) # increment the idx to keep track of queries left
 		totalWaitingTime += duration * queriesLeft
-		
-    return totalWaitingTime
+	
+	return totalWaitingTime
