97a0a9138af6171d9ae1233b8bd64412cef3235f
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-02-24 21:03:09 -0800

    updates

--- 3.algorithmic_expert/Greedy Algorithm/2.class_photos.py
+++ 3.algorithmic_expert/Greedy Algorithm/2.class_photos.py
@@ -1,14 +1,16 @@
 # all students wearing red shirts must be in a same row
 # all students		   blue shirts
-# students taller  than infront of them
-# if numbers same : return false
+# students taller are poistioned infront
+# if the elements are same : return false
 
 #O(nlogn) time - sort | O(1) space -> mutuating input array: if not then O(n) auxilary space
 def classPhotos(redShirtHeights, blueShirtHeights):
     redShirtHeights.sort(reverse=True)
 	blueShirtHeights.sort(reverse=True)
 	
-	shirtColorInFirstRow = "RED" if redShirtHeights[0] < blueShirtHeights[0] else "BLUE" # tallest student/ biggest number is added to blueShirtHeights followed by, redShirtHeights
+	# assign tallest element to either redShirtHeights array or blueShirtHeights array
+	shirtColorInFirstRow = "RED" if redShirtHeights[0] < blueShirtHeights[0] else "BLUE"
+	
 	for idx in range(len(redShirtHeights)):
 		redShirtHeight = redShirtHeights[idx]
 		blueShirtHeight = blueShirtHeights[idx]
