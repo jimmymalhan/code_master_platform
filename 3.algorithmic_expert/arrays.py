2d4613e8269c107c72a3832bfb86dcc330b547b3
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-03-06 08:01:15 -0800

    updates

---
+++ 3.algorithmic_expert/arrays.py
@@ -0,0 +1,35 @@
+# declare a variable - arrIdx = 0 and loop through to check arrIdx < len(array) or arrIdx == len(array)
+# for v in zip(*array) - > ([1,2],[3,4]) or slice [1:-1]
+# nested loops - for i in .. for j in i - Ques1
+
+# array = [1, 2, 3, 4]
+
+# # print(array) # [1, 2, 3, 4]
+# # for i in range(len(array) - 1):
+# # # for i in range(len(array)): # index out out bound
+# #     print(array[i]+array[i+1], end=' ') #3, 5, 7
+
+# # for i in range(len(array)):
+# #     print(array[i]) # 1, 2, 3, 4
+# #     # print(i) # 0, 1, 2, 3, 4
+
+# def range1(start, end):
+#     return range(start, end+1)
+# print(range1(1,10))
+
+#Ques 1- 
+# array = ([1,2],[3,4])
+# # output = 1, 3, 2, 4
+# newList = []
+# for v in zip(*array):
+#     # (1, 3)
+#     # (2, 4)
+#     for n in v:
+#         # print(n)
+#         # 1
+#         # 3
+#         # 2
+#         # 4
+#         newList.append(n)
+# print(newList)
+# # print(', '.join([str(n) for v in zip(*array) for n in v]))
