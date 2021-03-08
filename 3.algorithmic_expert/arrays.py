30e4419d0bd3eb7ee98ba7a022f1f2c480db714e
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-03-08 08:27:45 -0800

    updates

--- 3.algorithmic_expert/arrays.py
+++ 3.algorithmic_expert/arrays.py
@@ -1,13 +1,15 @@
 # declare a variable - arrIdx = 0 and loop through to check arrIdx < len(array) or arrIdx == len(array)
 # for v in zip(*array) - > ([1,2],[3,4]) or slice [1:-1]
 # nested loops - for i in .. for j in i - Ques1
+# triplets -  len(array) - 2
 
-# array = [1, 2, 3, 4]
-
-# # print(array) # [1, 2, 3, 4]
-# # for i in range(len(array) - 1):
-# # # for i in range(len(array)): # index out out bound
-# #     print(array[i]+array[i+1], end=' ') #3, 5, 7
+array = [1, 2, 3, 4]
+# for i in range(len(array) - 2):
+#     print(i)
+# for i in range(len(array) - 1):
+# for i in range(len(array)): # index out out bound
+    # print(array[i + 1]) # 2, 3, 4
+    # print(array[i]+array[i+1], end=' ') #3, 5, 7
 
 # # for i in range(len(array)):
 # #     print(array[i]) # 1, 2, 3, 4
