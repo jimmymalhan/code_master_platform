d835b9a42cc8c515f613962ed47c784a48aa4549
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-03-24 08:37:28 -0700

    updates

---
+++ 3.algorithmic_expert/Strings/4.generate_document.py
@@ -0,0 +1,16 @@
+characters = "Bste!hetsi ogEAxpelrt x "
+document = "AlgoExpert is the Best!"
+
+# n - len of char in string
+# m - len of doc in string
+def generateDocument(characters, document):
+    
+    print(characters)
+    # document.sort()
+    # print(document)
+	# if sorted(characters) == sorted(document) and sorted(characters) > sorted(document):
+	# 	return True
+	# else:
+	# 	return False
+
+print(generateDocument(characters, document))
