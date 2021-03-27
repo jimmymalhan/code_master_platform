bab8766fb4f8ce07f63b3b12f0c12725531d24a2
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-03-27 10:22:19 -0700

    updates

--- 3.algorithmic_expert/Strings/4.generate_document.py
+++ 3.algorithmic_expert/Strings/4.generate_document.py
@@ -1,16 +1,35 @@
-characters = "Bste!hetsi ogEAxpelrt x "
-document = "AlgoExpert is the Best!"
+# n - len of character in string
+# m - len of document in string
 
-# n - len of char in string
-# m - len of doc in string
+# Solution -> 1 counting elements in char and document everytime -> increses the time
+# O(m*(n + m)) space | O(1) space 
+# counting every element in character(n) + counting every element in document(m)
+# -> leads to (n + m)
+# every iteration of the document(m) -> will do (n + m) work
+# therefore, m*(n + m) time
+
+# Solution -> 2 -> create a set{}, after counting to char's ADD it to the set{}
+# -> which means we don't need to count again in set{}
+# O(c*(n+m)) time | O(c)space where, c is number of unique elements in the document
+
+# Solution -> 3 -> loop through only once, store the count in hashmap/dictionary 
+# as key/vaue store {} to check if we have enough elements to create document
+# O(n+m)time | O(c) space where c is number of unique elements in the document
+
+# O(m*(n + m)) space | O(1) space 
 def generateDocument(characters, document):
-    
-    print(characters)
-    # document.sort()
-    # print(document)
-	# if sorted(characters) == sorted(document) and sorted(characters) > sorted(document):
-	# 	return True
-	# else:
-	# 	return False
-
-print(generateDocument(characters, document))
+	for character in document:
+		documentFrequency = countCharacterFrequency(character, document)
+		charactersFrequency = countCharacterFrequency(character, characters)
+		if documentFrequency > charactersFrequency:
+			return False
+	return True
+	# document.count(character) # O(n)
+	# characters.count(character)
+def countCharacterFrequency(character, target):
+		frequency = 0
+		for char in target:
+			if char == character:
+				frequency += 1
+		return frequency
+		
