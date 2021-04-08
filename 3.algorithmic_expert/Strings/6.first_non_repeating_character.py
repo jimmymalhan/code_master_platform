3c9732a7dfd1ed556f3e036589cbc78a08f7c02e
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-04-08 13:20:23 -0700

    updates

--- 3.algorithmic_expert/Strings/6.first_non_repeating_character.py
+++ 3.algorithmic_expert/Strings/6.first_non_repeating_character.py
@@ -10,3 +10,19 @@ def firstNonRepeatingCharacter(string):
 			return i
 		
 	return -1
+
+# O(n) time | O(1) space
+def firstNonRepeatingCharacter(string):
+	characterFrequencies = {}
+	
+	for character in string:
+		characterFrequencies[character] = characterFrequencies.get(character, 0) + 1 
+		# it look in the hashMap/Dict - if character is not there(array) it assigns/add the value 0 + 1 = 1 
+		
+	
+	# idx traversal
+	for idx in range(len(string)):
+		character = string[idx]
+		if characterFrequencies[character] == 1: # checking the first non-repeating character in hashMap
+			return idx
+	return -1
