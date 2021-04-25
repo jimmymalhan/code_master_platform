000f2b38327cd88fe1115cced39ddc032e76dc64
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-04-25 10:57:13 -0700

    updates

--- 3.algorithmic_expert/Strings/8.valid_ip_addresses.py
+++ 3.algorithmic_expert/Strings/8.valid_ip_addresses.py
@@ -2,21 +2,21 @@
 def validIPAddresses(string):
 	ipAddressesFound = []
 	
-	for i in range(1, min(len(string), 4)):
+	for i in range(1, min(len(string), 4)): # from index 0 - 4
 		currentIPAddressParts = ["","","",""]
 		
-		currentIPAddressParts[0] = string[:i]
+		currentIPAddressParts[0] = string[:i] # before the first period
 		if not isValidPart(currentIPAddressParts[0]):
 			continue
 			
-		for j in range(i + 1, i + min(len(string) - i, 4)):
-			currentIPAddressParts[1] = string[i:j]
+		for j in range(i + 1, i + min(len(string) - i, 4)): # i + 1 = for second period, placement of i at most 3 positions past of i 
+			currentIPAddressParts[1] = string[i : j] # start from index i where the first position started to j at placement
 			if not isValidPart(currentIPAddressParts[1]):
 				continue
 			
 			for k in range(j + 1, j + min(len(string) - j, 4)):
-				currentIPAddressParts[2] = string[j:k]
-				currentIPAddressParts[3] = string[k:]
+				currentIPAddressParts[2] = string[j:k] # 3rd section
+				currentIPAddressParts[3] = string[k:] # 4th section
 				
 				if isValidPart(currentIPAddressParts[2]) and isValidPart(currentIPAddressParts[3]):
 					ipAddressesFound.append(".".join(currentIPAddressParts))
@@ -28,4 +28,4 @@ def isValidPart(string):
 	if stringAsInt > 255:
 		return False
 	
-	return len(string) == len(str(stringAsInt)) # check for leading 0
+	return len(string) == len(str(stringAsInt)) # check for leading 0 # 00 converted to  0, 01 converted to 1
