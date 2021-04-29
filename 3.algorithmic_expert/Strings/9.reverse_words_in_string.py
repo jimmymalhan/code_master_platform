2f7c965012b6e7ec7cace0f8b0e3f0f1e15a0d9e
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-04-29 07:47:35 -0700

    updates

--- 3.algorithmic_expert/Strings/9.reverse_words_in_string.py
+++ 3.algorithmic_expert/Strings/9.reverse_words_in_string.py
@@ -1,7 +1,7 @@
 def reverseWordsInString(string):
 	return " ".join(reversed(string.split()))
 
-# O(n) time | O(n) space
+# O(n) time | O(n) space - where n is the length of the string
 def reverseWordsInString(string):
 	words = []
 	startOfWord = 0
@@ -21,6 +21,30 @@ def reverseWordsInString(string):
 
 def reverseList(list):
 	start, end = 0, len(list) - 1
+	while start < end:
+		list[start], list[end] = list[end], list[start]
+		start += 1
+		end -= 1
+
+# ====================================================================
+
+# O(n) time | O(n) space - where n is the length of the string
+def reverseWordsInString(string):
+	characters = [char for char in string]
+	reverseListRange(characters, 0, len(characters) - 1)
+	
+	startOfWord = 0
+	while startOfWord < len(characters):
+		endOfWord = startOfWord
+		while endOfWord < len(characters) and characters[endOfWord] != " ":
+			endOfWord += 1
+			
+		reverseListRange(characters, startOfWord, endOfWord - 1)
+		startOfWord = endOfWord + 1
+	
+	return "".join(characters)
+
+def reverseListRange(list, start, end):
 	while start < end:
 		list[start], list[end] = list[end], list[start]
 		start += 1
