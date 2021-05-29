8b60f23cc32d9c2b011940d641801f6a8440c915
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-05-29 16:22:07 -0700

    testing

--- 3.algorithmic_expert/Strings/test.py
+++ 3.algorithmic_expert/Strings/test.py
@@ -1,23 +1,4 @@
 words = ["this", "that", "did", "deed", "them!", "a"]
 
 def minimumCharacter(words):
-    maximumCharacterFrequencies = {}
-
-    for word in words:
-        characterFrequencies = countCharacterFrequencies
-        updateMaximumFrequencies(characterFrequencies, maximumCharacterFrequencies)
-
-def countCharacterFrequencies(string):
-    characterFrequencies = {}
-
-    for character in string:
-        if character not in characterFrequencies:
-            characterFrequencies[character] = 0
-        characterFrequencies[character] += 1
-    return characterFrequencies
-
-def updateMaximumFrequencies(frequencies, maximumCharacterFrequencies):
-    for character in frequencies:
-        frequency = frequencies[character]
-
-        
+    
