920019ca264cfdb77d480733b60fd636fe55d799
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-05-31 10:20:53 -0700

    chore: todo update maximum frequencies

--- 3.algorithmic_expert/Strings/test.py
+++ 3.algorithmic_expert/Strings/test.py
@@ -6,7 +6,6 @@ def minimumCharactersForWords(words):
     maximumCharacterFrequencies = {}
     for word in words:
         characterFrequencies = countCharacterFrequencies
-        updateMaximumFrequencies(characterFrequencies, maximumCharacterFrequencies)
     # return countCharacterFrequencies
 
 def countCharacterFrequencies(string):
@@ -15,15 +14,6 @@ def countCharacterFrequencies(string):
         if character not in characterFrequencies:
             characterFrequencies[character] += 0
         characterFrequencies[character] += 1
-    return countCharacterFrequencies
-
-def updateMaximumFrequencies(frequencies, maximumFrequencies):
-    for character in frequencies:
-        frequency = frequencies[character]
-        
-        if character in maximumFrequencies:
-            maximumFrequencies[character] = max(frequency, maximumFrequencies[character])
-        else:
-            maximumFrequencies[character] = frequency    
+    return countCharacterFrequencies  
 
 print(minimumCharactersForWords(words))
