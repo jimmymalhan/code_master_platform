c6af2c10cbc3d4d74fe91a07fc6d4fb433518a57
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-05-31 20:01:37 -0700

    update

--- 3.algorithmic_expert/Strings/import_os.py
+++ 3.algorithmic_expert/Strings/import_os.py
@@ -3,8 +3,8 @@ S = os.listdir(os.getcwd())
 
 def solution(S):
     # S = os.listdir(os.getcwd())
-    S.sort(key=lambda f: os.stat(f).st_size, reverse=True)
-    S.sort(key=lambda a: os.stat(a).st_atime, reverse=True)
+    S.sort(key=lambda f: os.stat(f).st_size, reverse=True) # filesize
+    S.sort(key=lambda a: os.stat(a).st_atime, reverse=True) # last modified
     return S
 
 print(solution(S))
