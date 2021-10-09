bceee1efb5a3e0e29162e35099c998efd7b87b60
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-10-09 13:10:18 -0700

    chore

--- 3.algorithmic_expert/Strings/test.py
+++ 3.algorithmic_expert/Strings/test.py
@@ -1,14 +1,22 @@
 class MyClass:
-    def __init__(self, string:str) -> bool:
-        self.string = string
-    def isPalindrome(self):
-        reversedString = ""
-        for i in reversed(range(len(self.string))):
-            reversedString += self.string[i] # creating newString -> increases time
-        return self.string == reversedString
+	# def __init__(self, string, key):
+    # 	self.string = string
+    # 	self.key = key
+		
+	def caesarCipherEncryptor(self):
+		newLetters = []
+		newKey = self.key % 26 # edge case - when key (is BIG number) then, use - mod (%) 26 
+		alphabet = list("abcdefghijklmnopqrstuvwxyz")
+		for letter in self.string:
+			newLetters.append(getNewLetter(letter, newKey, alphabet))
+		return "".join(newLetters)
+
+	def getNewLetter(letter, key, alphabet):
+		newLetterCode = alphabet.index(letter) + self.key
+		return alphabet[newLetterCode % 26]
 
 def main():
-	print(MyClass("abcdcba").isPalindrome())
+	print(MyClass("xyz", 2).caesarCipherEncryptor())
 
 if __name__ == '__main__':
 	main()
