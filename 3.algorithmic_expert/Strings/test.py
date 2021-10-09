6e53e70df7db6e320e1a4cb26598bc70245cf0ed
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-10-09 12:42:07 -0700

    chore: adding classes

--- 3.algorithmic_expert/Strings/test.py
+++ 3.algorithmic_expert/Strings/test.py
@@ -1,12 +1,14 @@
-# read from backwards
-
 class MyClass:
     def __init__(self, string:str) -> bool:
         self.string = string
     def isPalindrome(self):
         reversedString = ""
         for i in reversed(range(len(self.string))):
-            reversedString += self.string[i]
+            reversedString += self.string[i] # creating newString -> increases time
         return self.string == reversedString
 
-print(MyClass("abcdcba").isPalindrome())
+def main():
+	print(MyClass("abcdcba").isPalindrome())
+
+if __name__ == '__main__':
+	main()
