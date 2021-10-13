36b5feefec8944e67fdf194bf7c479efa0da3cc1
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-10-13 11:29:49 -0700

    chore: update

--- 5.parsing_API/Requests-HTML/parse_html.py
+++ 5.parsing_API/Requests-HTML/parse_html.py
@@ -1,10 +1,16 @@
 from requests_html import HTML
 
 # Read the HTML file and print the results
-
-def Solution():
-    file = open("simple.html", "rt")
-    print(file.read())
+class MyClass:
+    def Solution():
+        # file = open("simple.html", "rt")
+        # print(file.read())
+        # or
+        
+        with open("simple.html") as html_file:
+            source = html_file.read()
+            html = HTML(html=source)
+        print(html.html)
 
 if __name__ == "__main__":
-    Solution()
+    MyClass.Solution()
