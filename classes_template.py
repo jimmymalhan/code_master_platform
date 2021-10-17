fbfd1f674b6f0fdd8e210c816a49d7b2eb513360
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-10-16 20:30:52 -0700

    chore: adding reusable template

---
+++ classes_template.py
@@ -0,0 +1,10 @@
+class Solution:
+    def __init__(self,string: str):
+        self.string = s
+
+    def toQuestion(self):
+        pass
+
+if __name__ == '__main__':
+    givenArray = Solution("enter_string")
+    print(givenArray.toQuestion())
