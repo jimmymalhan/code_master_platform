92829c12c8bca23a7ff0baf045b5474d1505d4ee
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-10-17 10:03:59 -0700

    fix: indentation

--- 3.algorithmic_expert/Graphs/1.depth_first_search.py
+++ 3.algorithmic_expert/Graphs/1.depth_first_search.py
@@ -4,21 +4,24 @@ class Node:
         self.name = name # root node
 
     def addChild(self, name):
-        self.children.append(Node(name)) # adding the name of the children
+        self.children.append(Node(name)) # adding the names of children
         return self
 
-# v - vertices
-# e - edges
-# O(v + e) time | O(v) space # v frames on call stack
+    # v - vertices
+    # e - edges
+    # O(v + e) time | O(v) space # v frames on call stack
+
     def depthFirstSearch(self, array):
         array.append(self.name) # adding root Node
         for child in self.children:# looping for every object in children
             child.depthFirstSearch(array) # calling recursively
         return array
+"""
+    Explanation:
 
-        #Example - 
-        # def depthFirstSearch(self, array):
-        # array.append(self.name) # Step - 1 start at rootNode and ADD - A | Step - 4 append B
-		# for child in self.children: # Step - 2 go to B | Step - 5 go to E
-		# 	child.depthFirstSearch(array) # Step - 3 call the stack for depthFIrst Search for B | step -6 so on and so forth
-		# return array # only used for first call which is returning root Node A
+    def depthFirstSearch(self, array):
+        array.append(self.name)          # Step - 1 begins at rootNode and ADD - 'A' | Step - 4 append 'B'
+        for child in self.children:      # Step - 2 go to 'B'                     | Step - 5 go to 'E'
+            child.depthFirstSearch(array)# Step - 3 call the stack in DFS for 'B' | step - 6 so on and so forth
+        return array
+"""
