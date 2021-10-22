6dee5111f6aa1b168084407de930b78799255c19
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-10-21 21:21:18 -0700

    chore: add data

--- 3.algorithmic_expert/Graphs/2.single_cycle_check.py
+++ 3.algorithmic_expert/Graphs/2.single_cycle_check.py
@@ -1,28 +1,27 @@
-# element at index 0 cannot be jumped through more than once and 
-# (n+1)th element jump must be the first element you visited i.e index 0
-
-# O(n) time | O(1) space - where n is the length of the input array
-
+# condition1 - element at index 0 cannot be jumped through more than once and 
+# condition2 - (n+1)th element jump must be the first element you visited i.e index 0
 
 class graph:
     def __init__(self, array):
         self.array = array
-    
+
+# O(n) time | O(1) space - where n is the length of the input array    
+
     def hasSingleCycle(self):
         numElementsVisited = 0
         currentIdx = 0
 
-        while numElementsVisited < len(self.array):
-            if numElementsVisited > 0 and currentIdx == 0:
+        while numElementsVisited < len(self.array): # condition1
+            if numElementsVisited > 0 and currentIdx == 0: # condition2
                 return False
             numElementsVisited += 1
             currentIdx = self.getNextIndex(currentIdx, self.array)
-        return currentIdx == 0
+        return currentIdx == 0 # the cycle should return back to index 0
     
     def getNextIndex(self, currentIdx, array):
         jump = self.array[currentIdx]
         # print(jump) # [2, 1, -4, 2, 3, -4]
-        nextIdx = (currentIdx + jump) % len(self.array)
+        nextIdx = (currentIdx + jump) % len(self.array) # '%' takes care of the -ve value
         # print(nextIdx) # [2, 3, 5, 1, 4, 0]
         return nextIdx if nextIdx >= 0 else nextIdx + len(self.array)
 
