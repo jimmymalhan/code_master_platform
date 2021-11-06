acf96c00c23cca56c9cbba9a6b96c0f249b79337
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-11-06 09:36:17 -0700

    chore: adding data

--- 5.parsing_API/fizz_buzz.py
+++ 5.parsing_API/fizz_buzz.py
@@ -6,9 +6,9 @@ class FizzBuzz:
         self.start = start
         self.end = end
 
-    def fizz_buzz(self):
+    def fizz_buzz_v1(self):
         for i in range(self.start, self.end + 1):
-            if i % 15 == 0:
+            if i % 3 == 0 and i % 5 == 0:
                 print('FizzBuzz')
             elif i % 3 == 0:
                 print('Fizz')
@@ -17,13 +17,14 @@ class FizzBuzz:
             else:
                 print(i)
     
+
     def fizz_buzz_v2(self):
         for i in range(self.start, self.end + 1):
-            print(('FizzBuzz' if i % 15 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 else i))
+            print(('FizzBuzz' if i % 3 == 0 and i % 5 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 else i))
 
 def main():
-    fizz_buzz = FizzBuzz()
-    # fizz_buzz.fizz_buzz()
+    fizz_buzz = FizzBuzz(1, 100)
+    # fizz_buzz.fizz_buzz_v1()
     fizz_buzz.fizz_buzz_v2()
 
 if __name__ == '__main__':
