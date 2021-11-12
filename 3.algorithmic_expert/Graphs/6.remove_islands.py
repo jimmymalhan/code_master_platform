4d78ad05e40ba74420707bf4e3164dfbf4a4124e
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-11-11 17:02:21 -0800

    chore: updating data

--- 3.algorithmic_expert/Graphs/6.remove_islands.py
+++ 3.algorithmic_expert/Graphs/6.remove_islands.py
@@ -32,7 +32,7 @@
 #     [1, 0, 0, 0, 0, 1],
 # ]
 
-# The islands that were removed can be clearlt seen here:
+# The islands that were removed can be clearly seen here:
 #   [
 #     [ ,  ,  ,  ,  ,  ],
 #     [ , 1,  ,  ,  ,  ],
@@ -41,11 +41,21 @@
 #     [ ,  , 1, 1,  ,  ],
 #     [ ,  ,  ,  ,  ,  ],
 # ]
+# Explanation on the question:
+#1. Remove islands(edge borders)
+#2. Remove islands = remove all the 1s that are not connected to the border horizontally or vertically (not diagonally)
 
 # Hints:
+# find the positions of all the non-island 1s and store. Use dfs on all the 1st that are on the border of the image. Afterwards, you can easily identify and remove all the islands 1s from the input matrix by relying on the DS that you used to store the positions of the non-island 1s.
 
+# You can also solve this problem without the use of a DS that stores the positions of the non-island 1s. Simply loop through the border of the image, and perform DFS on all positions with the value 1. Find all the 1s that are connected to the original position on the border, and change them from 1 to 2. After changing all non-island 1s to 2s, you can simply remove all the remaining 1s which are guranteed to be islands, from the matrix(by replacing them with 0s), and you can then change all the 2s back to 1s, since these were previously determined to be non-islands.
 
 # Explanation of the solution:
 #   We start by iterating through the matrix and marking all the 1s that are not touching the border of the matrix.
 #   Then, we iterate through the matrix again and replace all the 1s that are touching the border with 0s.
 
+# O(wh) time | O(wh) space - where w and h are the width and height of the matrix
+
+class solution:
+    def removeIslands(self, matrix):
+        return []
