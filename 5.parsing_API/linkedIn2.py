14fd322b82bde56cb0df8180dcb93f884d89d705
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-11-16 06:39:23 -0800

    chore: adding data

--- 5.parsing_API/linkedIn2.py
+++ 5.parsing_API/linkedIn2.py
@@ -1,72 +1,3 @@
-# Q2
-# Assume there is a REST API available at "http://www.linkedin.corp/api" for accessing employee information. The employee information endpoint is "/employee/<id>". Each employee record you retrieve will be a JSON object with the following keys:
-
-#   - 'name'  refers to a String that contains the employee's first and last name
-
-#   - 'title' refers to a String that contains the employee's job title
-
-#   - 'reports' refers to an Array of Strings containing the IDs of the employee's direct reports
-
-
-# Sample JSON API Response:
-
-# # GET /employee/A123456789
-# {
-#  "name": "Flynn Mackie",
-#  "title": "Senior VP of Engineering",
-#  "reports": ["A123456793", "A1234567898"]
-# }
-# Write a function that will take an employee ID and print out the entire hierarchy, rooted at that employee, and including all employees under that employee.
-
-# For example, suppose that Flynn Mackie's employee id is 'A123456789' and his only direct reports are Wesley Thomas and Nina Chiswick. If you provide 'A123456789' as input to your function, you will see the sample output below.
-
-
-# -----------Begin Sample Output--------------
-# Flynn Mackie - Senior VP of Engineering
-#   Wesley Thomas - VP of Design
-#     Randall Cosmo - Director of Design
-#       Brenda Plager - Senior Designer
-#   Nina Chiswick - VP of Engineering
-#     Tommy Quinn - Director of Engineering
-#       Jake Farmer - Frontend Manager
-#         Liam Freeman - Junior Software Engineer
-#       Sheila Dunbar - Backend Manager
-#         Peter Young - Senior Code Cowboy
-# -----------End Sample Output--------------
-
-import requests
-from requests.exceptions import HTTPError
-
-class Employee:
-    def __init__(self, name, title, reports):
-        self.name = name
-        self.title = title
-        self.reports = reports
-
-    def api_fetcher(self, id):
-        try:
-            response = requests.get('http://www.linkedin.corp/api/employee/{}'.format(id))
-            response.raise_for_status()
-        except HTTPError as http_err:
-            print(f'HTTP error occurred: {http_err}')
-        except Exception as err:
-            print(f'Other error occurred: {err}')
-        else:
-            return response.json()
-
-    def print_hierarchy(self, id):
-        employee = self.api_fetcher(id)
-        print(employee['name'] + ' - ' + employee['title'])
-        for report in employee['reports']:
-            self.print_hierarchy(report) # recursive function to print hierarchy
-
-def main():
-    employee = Employee('', '', [])
-    employee.print_hierarchy('A123456789') # call print_hierarchy function
-
-if __name__ == '__main__':
-    main()
-
 #######################################################################################
 # Q3
 """
@@ -109,10 +40,16 @@ class LogParser(object):
         self.log_file = log_file 
         self.log_dict = {} 
 
+# split the log_file into lines
+# if the line has more than one word
+# get the first 13 characters # Jan 20 03:25:08
+# if the minute is not in the log_dict
+# increment the value of the minute in the log_dict
+
     def parse_log(self):
         with open(self.log_file, 'r') as f:
             for line in f:
-                line_split = line.split() # split the line into a list
+                line_split = line.split()
                 if len(line_split) > 1: # if the line has more than one word
                     timestamp = line_split[0] # get the first word
                     minute = timestamp[:13] # get the first 13 characters # Jan 20 03:25:08
@@ -122,6 +59,7 @@ class LogParser(object):
                         self.log_dict[minute] += 1 # increment the value of the minute in the log_dict
         return self.log_dict
 
+# write the minute and number_of_messages to the csv
     def write_csv(self, csv_file):
         with open(csv_file, 'w') as f:
             writer = csv.writer(f) # create a csv.writer
