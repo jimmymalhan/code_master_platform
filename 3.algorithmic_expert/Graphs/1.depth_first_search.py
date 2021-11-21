a3d71d0b888ce10d27785b69c00320095510db45
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-11-21 11:38:55 -0800

    chore: adding data

--- 3.algorithmic_expert/Graphs/1.depth_first_search.py
+++ 3.algorithmic_expert/Graphs/1.depth_first_search.py
@@ -1,3 +1,9 @@
+# https://www.algoexpert.io/questions/Depth-first%20Search
+
+# You're given a Node class that has a name and an array of optional children nodes. When put together, nodes form a acyclic tree-like structure.
+
+# Implement the depthFirstSearch method on the Node class, which takes in an empty array, traverses the tree using the Depth-First Search approach (specifically navigating the tree from left to right), stores all of the nodes' names in the array, and returns it.
+
 class Node:
     def __init__(self, name):
         self.children = [] # list of children
