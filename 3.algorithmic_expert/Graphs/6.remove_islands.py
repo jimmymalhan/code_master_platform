d99b99636e895dce0c52ff8ef2c332716d6cfc27
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-11-26 13:51:22 -0800

    chore: adding data

--- 3.algorithmic_expert/Graphs/6.remove_islands.py
+++ 3.algorithmic_expert/Graphs/6.remove_islands.py
@@ -130,18 +130,69 @@ def getNeighbors(matrix, row, col):
 ####################################
 # C) Optimal Solution: Better Average space complexity
 ####################################
-# Don't need to create Auxilary DS
-# 1. Loop through the EXTERIOR/BORDER of the image and find all of the 1s which are connected to the border and change them to 2.
-    ## DFS:
-        # Check Vertically and Horizontally if the position is 0, then we skip the position.
-        # if the position is 1, then we mark the position as 2, and perform DFS on the position horizontally and vertically if they are 1 as well (not diagonally) and mark them as 2 as well (so that we don't visit them again).
-# 2. Loop through the matrix, we simply change all the 2s back to 1s and change all the 1s to 0s.
+"""
+Don't need to create Auxilary DS
+1. Loop through the EXTERIOR/BORDER of the image and find all of the 1s which are connected to the border and change them to 2.
+    # DFS:
+        Check Vertically and Horizontally if the position is 0, then we skip the position.
+        if the position is 1, then we mark the position as 2, and perform DFS on the position horizontally and vertically if they are 1 as well (not diagonally) and mark them as 2 as well (so that we don't visit them again).
+2. Loop through the matrix, we simply change all the 2s back to 1s and change all the 1s to 0s.
 
-# O(wh) time | O(wh) space - where w and h are the width and height of the matrix
+O(wh) time | O(wh) space - where w and h are the width and height of the matrix
 
-# Its same space complexity as we need to perform DFS on the matrix using Stack. Suppose if we have all 1s in the matrix, then we need to perform DFS on all the 1s, so the space complexity is O(wh) and stack needs to store all the positions of the 1s.
+Its same space complexity as we need to perform DFS on the matrix using Stack. Suppose if we have all 1s in the matrix, then we need to perform DFS on all the 1s, so the space complexity is O(wh) and stack needs to store all the positions of the 1s.
 
-# Has better Average Space Complexity than the other two solutions
+Has better Average Space Complexity than the other two solutions
+
+##################
+function for removeIsIslands for matrix
+    loop for row as range to len of matrix
+        loop for col as range to len of matrix
+            rowIsBorder is equal to row is equalized to 0 or row is equalized to len of matrix - 1 # to check if row is border
+            colIsBorder is equal to col is equalized to 0 or col is equalized to len of matrix for row - 1 # to check if col is border
+            if not isBorder # if the position is not a border
+                continue
+            if matrix[row][col] is not equal to 1
+                continue
+            changeOnesConnectedToBorderToTwos for matix, row, col # change all ones connected to border to twos using DFS
+
+    loop for row as range to len of matrix
+        loop for col as range to len of matrix for row
+            color is equal to matrix[row][col] # color of the current position
+            if color is equalized to 1
+                matrix[row][col] is equal to 0 # change color to 0
+            if color is equalized to 2
+                matrix for row,col is equal to 1 # change color to 1
+    return matrix
+
+function changeOnesConnectedToBorderToTwos for matrix, row, col
+    stack is equal to [(startRow, StartCol)] # stack to store the positions to be checked
+    while len of stack is greater than 0
+        currentPOsition = stack.pop() # pop the last position from the stack
+        currentRow, currentCol = currentPosition # get the row and col of the current position
+        matrix for [currentRow][currentCol] is equal to 2 # change the color of the current position to 2
+        neighbors is equal to getNeighbors for matrix, currentRow, currentCol # get the neighbors of the current position
+        loop for neighbor in neighbors
+            row, col = neighbor # get the row and col of the neighbor
+            if matrix[row][col] is not equal to 1
+            stack.append(neighbor) # add the neighbor to the stack
+
+function getNeighbors for matrix, row, col
+    neighbors is equal to [] # list to store the neighbors
+    numRows is equal to len of matrix
+    numCols is equal to len of matrix for row
+
+    if row - 1 is greater than and equal to 0:
+        neighbors.append((row - 1, col)) # add the neighbor to the list # `UP`
+    if row + 1 is less than and equal to numRows:
+        neighbors.append((row + 1, col)) # add the neighbor to the list # `DOWN`
+    if col - 1 is greater than and equal to 0:
+        neighbors.append((row, col - 1)) # add the neighbor to the list # `LEFT`
+    if col + 1 is less than and equal to numCols:
+        neighbors.append((row, col + 1)) # add the neighbor to the list # `RIGHT`
+    return neighbors
+"""
+
 
 def removeIslandsOptimized(matrix):
     # Find all the 1s that are not islands
@@ -150,13 +201,13 @@ def removeIslandsOptimized(matrix):
             rowIsBorder = row == 0 or row == len(matrix) - 1 # check if the row is on the border 
             colIsBorder = col == 0 or col == len(matrix[row]) - 1 # check if the col is on the border
             isBorder = rowIsBorder or colIsBorder # check if the position is on the border
-            if not isBorder:
+            if not isBorder: # if the position is not on the border
                 continue
 
             if matrix[row][col] != 1:
                 continue # skip the position if it's not a 1
 
-            changeOnesConnectedToBorderToTwos(matrix, row, col) # perform DFS on all the 1s that are connected to the border
+            changeOnesConnectedToBorderToTwos(matrix, row, col) # change all ones connected to border to twos using DFS
 
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
@@ -182,7 +233,7 @@ def changeOnesConnectedToBorderToTwos(matrix, startRow, startCol):
             row, col = neighbor # unpack the neighbor # row = neighbor[0], col = neighbor[1]
             if matrix[row][col] != 1:
                 continue # skip the neighbor if it is not a 1
-            stack.append(neighbor) # push the neighbor into the stack
+            stack.append(neighbor) # add the neighbor into the stack
 
 def getNeighbors(matrix, row, col):
     neighbors = []
@@ -209,7 +260,7 @@ def main():
         [1, 0, 1, 1, 0, 0],
         [1, 0, 0, 0, 0, 1],
     ]
-    print(removeIslands(matrix))
+    # print(removeIslands(matrix))
     print(removeIslandsOptimized(matrix))
 
 if __name__ == "__main__":
