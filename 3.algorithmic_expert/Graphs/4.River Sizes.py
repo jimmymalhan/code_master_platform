9d740579d23a604160e9dc705b3945f672dca989
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-11-28 16:05:16 -0800

    chore: updating data

--- 3.algorithmic_expert/Graphs/4.River Sizes.py
+++ 3.algorithmic_expert/Graphs/4.River Sizes.py
@@ -40,7 +40,7 @@ stacks = depth first search
 Explain the Solution:
 1. Return the sizes of the rivers horizontally and vertically adjacent 1s in as traversing the matrix. Treat matrix as a graph where each 
 element is a node with upto 4 neighbors nodes(left, right1, top, bottom) and traverse the graph using DFS.
-2. While traversing the matrix, anytime you encounter a 1, taverse entire river that this 1 is part of(keep track of the size of the river) 
+2. While traversing the matrix, anytime you encounter a 1, traverse entire river that this 1 is part of(keep track of the size of the river) 
 by node's neighboring nodes and their own neighboring nodes as long as they are 1s. if you encounter a 0, mark it as visited and continue.
 3. In order to prevent mistakenly calculating the size of the river multiple times, use auxiliary data structure "queue" to perform computation on unvisited nodes only.
 O(wh) Time | O(wh) Space - width, height of matrix
