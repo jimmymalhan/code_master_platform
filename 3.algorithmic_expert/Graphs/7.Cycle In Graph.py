3cd33217833e061dc7e46036b41d8d5dd96e8d65
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-12-01 19:51:04 -0800

    chore: adding data

--- 3.algorithmic_expert/Graphs/7.Cycle In Graph.py
+++ 3.algorithmic_expert/Graphs/7.Cycle In Graph.py
@@ -3,7 +3,8 @@
 # Problem Name: Cycle In Graph
 
 # Problem Description:
-# You're given a list of edges representing an unweighted, directed graph with at least one node. Write a function that returns a boolean representing whether the given graph contains a cycle.
+
+# You're given a list of edges representing an ```unweighted, directed graph```` with at least one node. Write a function that returns a boolean representing whether the given graph contains a cycle.
 
 # For the purpose of this question, a cycle is defined as any number of vertices, including just one vertex, that are connected in a closed chain. A cycle can also be defined as a chain of at least one vertex in which the first vertex is the same as the last.
 
@@ -30,7 +31,18 @@
 # Thse are just 3 examples; there are more.
 ####################################
 """
-Explain the solution:
+Explanation:
+- Graph is directed and unweighted.
+
+- Directed means that if there is a path from A to B, you can't go from B to A.
+- unweighted means that there is no difference between the weight of edges and distance between nodes.
+
+- If it's cycle, return True and it can have mutiple cycles.
+- cycle - closed chain for vertices that are connected in together
+eg - 0 -> 1 -> 2 -> 0
+- self loop - 0 -> 0, represents a cycle
+
+Example the Solution:
 - When traversing a graph using DFS, a back edge is an edge from a node to one of its ancestors in the DFS tree, and a back edge denotes the presence of a cycle.  How can you determine if a graph has any back edges?
 - To find back edges, you'll need to keep track of which nodes you've already visited and which nodes are ancestors of the current node in the DFS tree. There a few ways to do this, but one approach is to recursively traverse the graph and to keep track of which nodes have been visited in general and which nodes have visited in the current recursion stack; you can do so with two separate data structures. If you reach a node that has an edge to a nnode that's already in the recursion stack, then you've detected a back edge, and there's a cycle in the graph.
 - You can also detect a back edge by performing a 3-color DFS. Each node is colored white to start; recursively traverse through the graph, coloring the current node grey and then calling the recursive traversal function on all of its neighbors. After traversing all the neighbors, color the current node black to signify that it's been visited. IF you ever find an edge to a node that's grey, you've found a back edge, and there's a cycle in the graph.
