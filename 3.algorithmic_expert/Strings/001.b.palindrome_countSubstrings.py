8dd62cc75314ccb844fc904d2bdb85e43cea2f67
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-12-22 07:06:14 -0800

    chore: adding data

--- 3.algorithmic_expert/Strings/001.b.palindrome_countSubstrings.py
+++ 3.algorithmic_expert/Strings/001.b.palindrome_countSubstrings.py
@@ -1,6 +1,7 @@
 # Count the number of substrings of s that contain exactly k distinct characters.
-# O(n^2) time | O(n) space
+# https://leetcode.com/problems/palindromic-substrings/
 
+# O(n^2) time | O(n) space
 class Solution:
     def countSubstrings(self, s):
         count = 0
@@ -12,6 +13,14 @@ class Solution:
     
     def isPal(self,s):
         return s==s[::-1]
+
+    def countSubstrings(self, s: str) -> int:
+	    L, r = len(s), 0 # L is the length of the string, r is the number of palindromes
+	    for i in range(L): 
+	    	for a,b in [(i,i),(i,i+1)]: # a and b are the start and end indices of the substring
+	    		while a >= 0 and b < L and s[a] == s[b]: a -= 1; b += 1
+	    		r += (b-a)//2
+	    return r
 
 def main():
     stringName = Solution()
