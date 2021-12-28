41eb800e3a2d9bebde3267918dfb8da8d19a6d93
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-12-28 07:46:27 -0800

    chore: adding data

--- 3.algorithmic_expert/Graphs/8.Minimum Passes Of Matrix.py
+++ 3.algorithmic_expert/Graphs/8.Minimum Passes Of Matrix.py
@@ -67,19 +67,71 @@ def minimumPassesOfMatrix1(matrix):
 
 def convertNegatives(matrix):
     nextPassQueue = getAllPositivePositions(matrix)
+	
     passes = 0
+    
     while len(nextPassQueue) > 0:
         currentPassQueue = nextPassQueue
         nextPassQueue = []
+		
         while len(currentPassQueue) > 0:
-            # In Python, popping elements
+            # In Python, popping elements from the start of a list is an O(n) operation.
+            # To make this an O(1) operation, we could use the `deque` object.
+            # For our time complexity analysis, we'll assume this runs in O(1) time.
+            # Also, for this particular solution, we could actually just turn this queue into a stack and replace `.pop(0)` with the constant-time `.pop()` operation.
+            currentRow, currentCol = currentPassQueue.pop(0)
+            
+            adjacentPositions = getAdjacentPositions( currentRow, currentCol, matrix)
+            for position in adjacentPositions:
+                row, col = position
+                
+                value = matrix[row][col]
+                if value < 0:
+                    matrix[row][col] *= -1
+                    nextPassQueue.append([row, col])
+					
+        passes += 1
+		
+    return passes
+
+def getAllPositivePositions(matrix):
+	positivePositions = []
+
+	for row in range(len(matrix)):
+		for col in range(len(matrix[row])):
+			value = matrix[row][col]
+			if value > 0:
+				positivePositions.append([row, col])
+	return positivePositions
+
+def getAdjacentPositions(row, col, matrix):
+	adjacentPositions = []
+	
+	if row > 0:
+		adjacentPositions.append([row - 1, col])
+	if row < len(matrix) - 1:
+		adjacentPositions.append([row + 1, col])
+	if col > 0:
+		adjacentPositions.append([row, col - 1])
+	if col < len(matrix[0]) - 1:
+		adjacentPositions.append([row, col + 1])
+	
+	return adjacentPositions
+
+def containsNegative(matrix):
+	for row in matrix:
+		for value in row:
+			if value < 0:
+				return True
+	return False
+
 def main():
     matrix = [
         [0, -1, -3, 2, 0],
         [1, -2, 5, -1, -3],
         [3, 0, 0, -4, -1],
     ]
-    print(minimumPassesOfMatrix1(matrix)) # 3
+    print(minimumPassesOfMatrix1(matrix))
 
 if __name__ == "__main__":
     main()
