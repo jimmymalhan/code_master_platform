14fb011735e4d7bff3f52bc8a739f7e315db9ea0
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-01-03 06:53:02 -0800

    chore: adding data

--- 3.algorithmic_expert/Graphs/8.Minimum Passes Of Matrix.py
+++ 3.algorithmic_expert/Graphs/8.Minimum Passes Of Matrix.py
@@ -50,22 +50,70 @@ Explain the solution:
     - TWO queue process:
     - Implementing a BFS, starting from all the positive-value positions in the array. Initialize a queue that stores the positions of all the positive values, iterate through the queue, dequeue elements out, and consider all of their neighbors. If any of their neighbors are negative, change them to positive, and store their positions in a secondary queue. Once the first queue is empty, Increment your number of passes, and iterate through the second queue you created(the one with the positions of negatives that were changed to positive). Repeat this process until no values are converted during a pass.
 
-3- ONE queue process:
-- Differentiate the values that were already positive when the current pass started from the values that were changed to positive during the current pass.
-
-    Meaning -> which values are processing in the current pass and which values are processing in the next pass.
-
 # O(w * h) time | O(w * h) space where - w is the width of the matrix and h is the height of the matrix
 
 ##################
 Detailed explanation of the Solution:
 
+create a function for minimumPassesOfMatrix for matrix:
+    intialize a variable passes equal to convertNegatives for matrix
+    return passes - 1 if not containsNegative for matrix # if there is no negative value in the matrix, return -1
+
+create a function for convertNegatives for matrix:
+    nextPassQueue equal to getAllPositivePositions for matrix
+    passes equal to 0
+
+    while length of nextPassQueue is greater than 0:
+        currentPassQueue equal to nextPassQueue # set the current pass queue to the next pass queue
+        nextPassQueue equal to [] # reset the next pass queue
+
+        while length of currentPassQueue is greater than 0: # while there are still values in the current pass queue
+            currentRow, currentCol equal to currentPassQueue.pop(0) # In Python, popping elements from the start of a list is an O(n) operation. # To make this an O(1) operation, we could use the `deque` object. # For our time complexity analysis, we'll assume this runs in O(1) time. # Also, for this particular solution, we could actually just turn this queue into a stack and replace `.pop(0)` with the constant-time `.pop()` operation.
+
+            adjacentPositions equal to getAdjacentPositions for currentRow, currentCol, matrix # get the adjacent positions of the current position
+            for position in adjacentPositions: # iterate through the adjacent positions
+                row, col equal to position # get the row and col of the adjacent position
+
+                value equal to matrix[row][col] # get the value of the adjacent position
+                if value < 0:
+                    matrix[row][col] equal to value * -1 # change the value of the adjacent position to -1
+                    nextPassQueue.append(position) # add the adjacent position to the next pass queue
+        passes += 1 # increment the number of passes
+    return passes
+
+create a function for getAllPositivePositions for matrix:
+    positivePositions equal to []
+    for row in range of length of matrix:
+        for col in range of length of matrix[row]:
+            value equal to matrix[row][col]
+            if value > 0:
+                positivePositions.append([row, col])
+    return positivePositions
+
+create a function for getAdjacentPositions for row, col, matrix:
+    adjacentPositions equal to []
+    if row > 0:
+        adjacentPositions.append([row - 1, col]) # up
+    if row < len(matrix) - 1:
+        adjacentPositions.append([row + 1, col]) # down
+    if col > 0:
+        adjacentPositions.append([row, col - 1]) # left
+    if col < len(matrix[row]) - 1:
+        adjacentPositions.append([row, col + 1]) # right
+    return adjacentPositions
+
+create a function for containsNegative for matrix:
+    for row in range of matrix:
+        for value in row:
+            if value < 0:
+                return True
+    return False
 """
 ####################################
 # Two queue process
 def minimumPassesOfMatrix_2_queue(matrix):
     passes = convertNegatives(matrix)
-    return passes - 1 if not containsNegative(matrix) else - 1
+    return passes - 1 if not containsNegative(matrix) else - 1 # if there is no negative value in the matrix, return -1
 
 def convertNegatives(matrix):
     nextPassQueue = getAllPositivePositions(matrix)
@@ -76,16 +124,16 @@ def convertNegatives(matrix):
         currentPassQueue = nextPassQueue # set the current pass queue to the next pass queue
         nextPassQueue = []               # reset the next pass queue      
 		
-        while len(currentPassQueue) > 0:
+        while len(currentPassQueue) > 0: # while there are still values in the current pass queue
+            currentRow, currentCol = currentPassQueue.pop(0)
             # In Python, popping elements from the start of a list is an O(n) operation.
             # To make this an O(1) operation, we could use the `deque` object.
             # For our time complexity analysis, we'll assume this runs in O(1) time.
             # Also, for this particular solution, we could actually just turn this queue into a stack and replace `.pop(0)` with the constant-time `.pop()` operation.
-            currentRow, currentCol = currentPassQueue.pop(0)
             
             adjacentPositions = getAdjacentPositions( currentRow, currentCol, matrix)
             for position in adjacentPositions:
-                row, col = position
+                row, col = position # get the row and col of the adjacent position
                 
                 value = matrix[row][col]
                 if value < 0:
@@ -129,6 +177,11 @@ def containsNegative(matrix):
 
 ####################################
 # One queue process
+# 3- ONE queue process:
+# - Differentiate the values that were already positive when the current pass started from the values that were changed to positive during the current pass.
+
+#     Meaning -> which values are processing in the current pass and which values are processing in the next pass.
+
 # O(w * h) time | O(w * h) space where - w is the width of the matrix and h is the height of the matrix
 def minimumPassesOfMatrix_1_queue(matrix):
     passes = convertNegatives(matrix)
