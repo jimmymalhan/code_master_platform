62258012749ca297f59467810255fc6d70f77050
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-01-10 07:49:14 -0800

    chore: adding data

--- 3.algorithmic_expert/Tries/1.Suffix Trie Construction.py
+++ 3.algorithmic_expert/Tries/1.Suffix Trie Construction.py
@@ -30,11 +30,11 @@
 ####################################
 """
 Explain the solution:
-- Building a suffix-trie-like data structure consists of essentially storing every suffic of a given string in a trie. To do so, iterate through the input string one character at a time, and insert every substring starting at each character and ending at the end of string into the trie.
+- Building a suffix-trie-like data structure consists of essentially storing every suffix of a given string in a trie. To do so, iterate through the input string one character at a time, and insert every substring starting at each character and ending at the end of string into the trie.
 
-- To insert a string into the trie, start by adding the first character of the string into the root node of the trie. and mapping it to an empty hash table if it isin't already there. Then, iterate through the rest of the string inserting each of the remaining characters into the previous character's corresponding node(or hash table) in the trie, making sure to add an endSymbol "*" at the end.
+- To insert a string into the trie, start by adding the first character of the string into the root node of the trie and map it to an empty hash table if it isin't already there. Then, iterate through the rest of the string, inserting each of the remaining characters into the previous character's corresponding node(or hash table) in the trie, making sure to add an endSymbol "*" at the end.
 
-- Searching the trie for a specific string should follow a nearlt identical logic to the one used to add a string in the trie.
+- Searching the trie for a specific string should follow a nearly identical logic to the one used to add a string in the trie.
 
 # Creation: O(n^2) time | O(n^2) space - where n is the length of the input string
 # Searching: O(m) time | O(1) space - where m is the length of the input string
