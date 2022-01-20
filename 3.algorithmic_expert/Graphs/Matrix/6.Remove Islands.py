37460956eec6825ed6270bc25be90d13caa43e67
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-01-20 08:32:36 -0800

    chore: adding data

--- 3.algorithmic_expert/Graphs/Matrix/6.Remove Islands.py
+++ 3.algorithmic_expert/Graphs/Matrix/6.Remove Islands.py
@@ -45,21 +45,20 @@
 ####################################
 # A) Brute Force approach: O(n^2) time complexity and O(n) space complexity
 ####################################
-# loop the matrix and check if the current position -
-# If it is on the border of the matrix then perform DFS on the current position.
-# if the current position is 1, then check if it is not a neighbor of the border which is 1, then we can replace it with 0.
+# loop the matrix and check the current position -
+    # if it is on the border of the matrix then perform DFS on the current position.
+    # if the current position is 1, then check if it is not a neighbor of the border which is 1, then we can replace it with 0.
 
 ####################################
 # B) Optimal Solution than brute force > reduce the time complexity to O(n), where n is the number of 1s in the matrix
 ####################################
 # Build Auxilary Data Structure =>> Intialize False for all positions in the matrix.
-# 1. Loop through the EXTERIOR/BORDER of the image and find all of the 1s which are connected to the border and mark them as True (perform DFS on all the 1s).
-    ## DFS:
-        # Check Vertically and Horizontally if the position is 0, then we skip the position.
-        # if the position is 1, then we mark the position as True, and perform DFS on the position horizontally and vertically if they are 1 as well (not diagonally) and mark them as True as well (so that we don't visit them again).
+    # 1. Loop through the EXTERIOR/BORDER of the image and find all of the 1s which are connected to the border and mark them as True (perform DFS on all the 1s).
+        ## DFS:
+            # Check Vertically and Horizontally if the position is 0, then we skip the position.
+            # if the position is 1, then we mark the position as True, and perform DFS on the position horizontally and vertically if they are 1 as well (not diagonally) and mark them as True as well (so that we don't visit them again).
 
-# 2. Loop through INTERIOR/INNER positions(not on the border) and if the value is 1 and doesn't have the value True, then we replace it with 0. If the position is 1 and has the value True, then we skip the position.
-
+    # 2. Loop through INTERIOR/INNER positions(not on the border) and if the value is 1 and doesn't have the value True, then we replace it with 0. If the position is 1 and has the value True, then we skip the position.
 
 # O(wh) time | O(wh) space(coz of identical DS) - where w and h are the width and height of the matrix
 def removeIslands(matrix):
@@ -129,17 +128,17 @@ def getNeighbors(matrix, row, col):
 """
 Explain the Solution:
 Note: Don't need to create Auxilary DS
-1. Loop through the EXTERIOR/BORDER of the image and find all of the 1s which are connected to the border and change them to 2.
-    # DFS:
-        Check Vertically and Horizontally if the position is 0, then we skip the position.
-        if the position is 1, then we mark the position as 2, and perform DFS on the position horizontally and vertically if they are 1 as well (not diagonally) and mark them as 2 as well (so that we don't visit them again).
-2. Loop through the matrix, we simply change all the 2s back to 1s and change all the 1s to 0s.
+    1. Loop through the EXTERIOR/BORDER of the image and find all of the 1s which are connected to the border and change them to 2.
+        # DFS:
+            Check Vertically and Horizontally if the position is 0, then we skip the position.
+            If the position is 1, then we mark the position as 2, and perform DFS on the position horizontally and vertically if they are 1 as well (not diagonally) and mark them as 2 as well (so that we don't visit them again).
+    2. Loop through the matrix, we simply change all the 2s back to 1s and change all the 1s to 0s.
 
 O(wh) time | O(wh) space - where w and h are the width and height of the matrix
 
 Its same space complexity as we need to perform DFS on the matrix using Stack. Suppose if we have all 1s in the matrix, then we need to perform DFS on all the 1s, so the space complexity is O(wh) and stack needs to store all the positions of the 1s.
 
-Has better Average Space Complexity than the other two solutions
+    - It has better Average Space Complexity than the other two solutions
 
 ##################
 Detailed explanation of the Solution:
