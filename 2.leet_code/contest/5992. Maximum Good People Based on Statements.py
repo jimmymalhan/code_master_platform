296a738b7a2ef91fc024bb54cfb367742b6509cb
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-01-22 22:24:08 -0800

    chore: adding data

--- 2.leet_code/contest/5992. Maximum Good People Based on Statements.py
+++ 2.leet_code/contest/5992. Maximum Good People Based on Statements.py
@@ -12,21 +12,23 @@
 # Return the maximum number of people who can be good based on the statements made by the n people.
 
 from typing import List
+import itertools
+
 class Solution:
     def maximumGood(self, statements: List[List[int]]) -> int:
-        n = len(statements)
-        good = [0] * n
-        bad = [0] * n
-        for i in range(n):
-            for j in range(n):
-                if statements[i][j] == 1:
-                    good[i] += 1
-                elif statements[i][j] == 0:
-                    bad[i] += 1
-        ans = 0
-        for i in range(n):
-            if good[i] == 0:
-                ans = max(ans, bad[i])
+        ans, n = 0, len(statements)
+        for person in itertools.product([0, 1], repeat=n): # use itertools to create a list only contains 0 or 1
+            valid = True                                   # initially, we think the `person` list is valid
+            for i in range(n):
+                if not person[i]: continue                 # only `good` person's statement can lead to a contradiction, we don't care what `bad` person says
+                for j in range(n):
+                    if statements[i][j] == 2: continue     # ignore is no statement was made
+                    if statements[i][j] != person[j]:      # if there is a contradiction, then valid = False
+                        valid = False
+                        break                              # optimization: break the loop when not valid
+                if not valid:                              # optimization: break the loop when not valid
+                    break        
+            if valid: 
+                ans = max(ans, sum(person))                # count sum only when valid == True
         return ans
-
-print(Solution().maximumGood([[2,1,2],[1,2,2],[2,0,2]]))
+print(Solution().maximumGood([[2,1,2],[1,2,2],[2,0,2]])) # 3
