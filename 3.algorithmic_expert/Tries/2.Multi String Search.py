ea825ac536c11fa1b4f5032b30a7a4652fe94b7a
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-03-16 15:55:53 -0700

    chore: adding data

--- 3.algorithmic_expert/Tries/2.Multi String Search.py
+++ 3.algorithmic_expert/Tries/2.Multi String Search.py
@@ -31,6 +31,37 @@ Explain the solution:
 
 ##################
 Detailed explanation of the Solution:
+create a function multiStringSearchOptimized that takes in two parameters: bigString and smallStrings.
+	trie is equal to Trie() # creates a trie-like data structure.
+	loop for string in smallStrings:
+		insert string into trie
+	containedStrings is equal to {} # contains all of the small strings that are contained in the big string.
+	loop for i in range(len(bigString)):
+		findSmallStringsIn(bigString, i, trie, containedStrings)
+	return [string in containedStrings for string in smallStrings] # returns an array of booleans, where each boolean represents whether the small string at that index in the array of small strings is contained in the big string.
+
+create findSmallStringsIn function that takes in four parameters: string, startIdx, trie, and containedStrings.
+	currentNode is equal to trie.root
+	loop for i in range(startIdx, len(string)):
+		currentChar is equal to string[i]
+		if currentChar not in currentNode: # if the current character is not in the current node, break out of the loop.
+			break
+		currentNode is equal to currentNode[currentChar] # move to the next node.
+		if trie.endSymbol in currentNode:
+			containedStrings[currentNode[trie.endSymbol]] = True # add the small string to the containedStrings dictionary.
+
+class Trie:
+	def __init__(self):
+		self.root = {}
+		self.endSymbol = "*" # the end symbol is used to mark the end of a word.
+
+	create insert function that takes in self and string.
+		current is equal to self.root # start at the root.
+		loop for i in range(len(string)):
+			if string[i] not in current:
+				current[string[i]] = {} # if the current character is not in the current node, add it to the current node.
+			current = current[string[i]] # move to the next node.
+		current[self.endSymbol] = string # add the end symbol to the current node.
 
 """
 ####################################
@@ -105,11 +136,11 @@ class ModifiedSuffixTrie:
 
 # O(ns + bs) time | O(ns) space - where n is the number of small strings, s is the length of longest small sting, and b is the length of big string.
 
-def multiStringSearch(bigString, smallStrings):
-	trie = Trie()
+def multiStringSearchOptimized(bigString, smallStrings):
+	trie = Trie() # creates a trie-like data structure.
 	for string in smallStrings:
 		trie.insert(string)
-	containedStrings = {}
+	containedStrings = {} # contains all of the small strings that are contained in the big string.
 	for i in range(len(bigString)):
 		findSmallStringsIn(bigString, i, trie, containedStrings)
 	return [string in containedStrings for string in smallStrings]
@@ -118,23 +149,23 @@ def findSmallStringsIn(string, startIdx, trie, containedStrings):
 	currentNode = trie.root
 	for i in range(startIdx, len(string)):
 		currentChar = string[i]
-		if currentChar not in currentNode:
+		if currentChar not in currentNode: # if the current character is not in the current node, break out of the loop.
 			break
-		currentNode = currentNode[currentChar]
+		currentNode = currentNode[currentChar] # move to the next node.
 		if trie.endSymbol in currentNode:
 			containedStrings[currentNode[trie.endSymbol]] = True
 			
 class Trie:
 	def __init__(self):
 		self.root = {}
-		self.endSymbol = "*"
+		self.endSymbol = "*" # the end symbol is used to mark the end of a word.
 		
 	def insert(self, string):
 		current = self.root
 		for i in range(len(string)):
 			if string[i] not in current:
-				current[string[i]] = {}
-			current = current[string[i]]
-		current[self.endSymbol] = string
+				current[string[i]] = {} # if the current character is not in the current node, add it to the current node.
+			current = current[string[i]] # move to the next node.
+		current[self.endSymbol] = string # mark the end of the string.
 
-print(multiStringSearch("this is a big string", ["this", "yo", "is", "a", "bigger", "string", "kappa"])) # [True, False, True, True, False, True, False]
+print(multiStringSearchOptimized("this is a big string", ["this", "yo", "is", "a", "bigger", "string", "kappa"]))#[True, False, True, True, False, True, False]
