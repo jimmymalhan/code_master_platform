c091a885a4113f14177f6c7ee8d0958a128b3edc
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-04-10 19:03:27 -0700

    chore: adding data

---
+++ 3.algorithmic_expert/Greedy Algorithm/3.tandem_bicycle.py
@@ -0,0 +1,28 @@
+# Problem Name: Tandem Bicycle
+
+# Problem Description:
+# A tandem bicycle is a bicycle that's operated by two people: person A and person B. Both peope pedal the bicycle, but the person that pedals faster dictates the speed of the bicycle. So if person A is pedals at a speed of 5, and person B pedals at a speed of 4, the tandem bicycle moves at a speed of 5(i.e tandem speed = max(speedA, speedB)).
+
+# You're given two lists of positive integers: one that contains the speeds of riders wearing red-shirts and one that contains the speeds of riders wearing blue-shirts. Each rider is represented by a single positive integer, which is the speed that they pedal a tandem bicycle at. Both lists have the same length, meaning that there are as many red-shirt riders as there are blue-shirt riders. Your goal is to pair every rider wearing a red-shirt with rider wearing a blue-shirt to operate a tandem bicycle.
+
+# Write a function that returns the maximum possible total speed or the minimum possible total speed of all the tandem bicycles being ridden based on an input parameters, fastest. If fastest = True, your function should return the maximum possible total speed; otherwise it should return the minimum total speed. 
+
+# "Total Speed" is defined as the sum of the speeds of all the tandem bicycle being ridden. For example, if there are 4 riders (2 red-shirt riders and 2 blue-shirt riders) who have speeds of 1,3,4,5, and if they're paired on tandem bicycles as follows: [1, 4], [5, 3], then the total speed of these tandem bicyles is 4 + 5 = 9.
+  
+####################################
+# Sample Input:
+# Sample Output:
+####################################
+"""
+Explain the solution:
+
+##################
+Detailed explanation of the Solution:
+
+"""
+####################################
+
+def solution(self):
+    pass
+
+print(solution())
