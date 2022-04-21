85a86f19e9276e155589df9bab081206047e6a71
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-04-21 08:45:56 -0700

    chore: adding data

--- 3.algorithmic_expert/Arrays/10.monotonic_array.py
+++ 3.algorithmic_expert/Arrays/10.monotonic_array.py
@@ -26,30 +26,46 @@ Explain the solution:
 
 # O(n) time | O(1) space - where n is the length of the array.
 
-
 ##################
 Detailed explanation of the Solution:
+create function called isMonotonic that takes in an array of integers:
+    if len of array is less than or equal to 2, return True # if the array is empty or has only one element, it is monotonic.
+
+    inititalize direction equal by subtracting second element from first element.
+    iterate through the array from index 2 to the end of the array:
+        if direction is equal to 0: # if the array is entirely non-increasing or non-decreasing or equal
+            direction is equal to subtracting the current element from the previous one.
+            continue # continue to the next iteration of the loop.
+        if breaksDirection for direction, array[i-1], array[i]:
+            return False # if the direction breaks, return False.
+    return True # if the loop completes, return True.
+
+function breaksDirection takes in a direction, previousInt, and currentInt:
+    difference is equal to currentInt - previousInt # difference between the current and previous integers.
+    if direction is greater than 0: # if the array is non-decreasing
+        return difference is less than 0 # if the difference is negative, the array is not monotonic.
+    return difference is greater than 0 # if the difference is positive, the array is not monotonic.
 
 """
 ####################################
 
 def isMonotonic(array):
-    if len(array) <= 2:
+    if len(array) <= 2: # If the array is empty or has only one element, it is monotonic.
         return True
 
-    direction = array[1] - array[0]
+    direction = array[1] - array[0] # initialize direction to the trend of the array because we know that the first two elements are the only ones that can break the trend.
     for i in range(2, len(array)):
-        if direction == 0:
-            direction = array[i] - array[i - 1]
+        if direction == 0: # If the array is entirely non-increasing or non-decreasing or equal
+            direction = array[i] - array[i - 1] # updating direction by subtracting the current element from the previous one.
             continue
         if breaksDirection(direction, array[i -1], array[i]):
             return False
     return True
 
 def breaksDirection(direction, previousInt, currentInt):
-    difference = currentInt - previousInt
-    if direction > 0:
-        return difference < 0
-    return difference > 0
+    difference = currentInt - previousInt # difference between the current and previous integers.
+    if direction > 0: # If the array is non-decreasing
+        return difference < 0 # If the difference is negative, the array is not monotonic.
+    return difference > 0 # If the difference is positive, the array is not monotonic.
 
 print(isMonotonic([-1, -5, -10, -1100, -1100, -1101, -1102, -9001]))
