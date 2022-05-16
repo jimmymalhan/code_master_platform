69df4a05fe28fb218700ac855260292a07211f25
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-05-16 08:53:31 -0700

    adding data

---
+++ 3.algorithmic_expert/Arrays/12.longest_peak.py
@@ -0,0 +1,29 @@
+# Problem Name: Longest Peak
+
+# Problem Description:
+# Write a function that takes in an array of integers and returns the length of the longest peak in the array.
+
+# A peak is defined as adjacent integers in the array that are strictly increasing until they reach a tip(the highest value in the peak), at which point they become strictly decreasing. Atleast three integers are required to form a peak.
+
+# For example, the integers `1, 4, 10, 2` form a peak, but the integers `4, 0, 10` don't and neither do the integers `1, 2, 2, 0``. Similarly, the integers `1, 2, 3`  don't form a peak because there aren't any strictly decreasing integers after the 3.
+
+####################################
+# Sample Input:
+# array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
+
+# Sample Output:
+# 6 # Because 0, 10, 6, 5, -1, -3 is the longest peak.
+
+####################################
+"""
+Explain the solution:
+
+##################
+Detailed explanation of the Solution:
+
+"""
+####################################
+
+def longestPeak(array):
+    # Write your code here.
+    pass
