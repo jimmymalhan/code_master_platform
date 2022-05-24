80bd8a0f8c8cf88ad36e6a006e8acd5f6e0bd2ab
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-05-24 09:25:06 -0700

    chore: adding data

--- 3.algorithmic_expert/Arrays/12.longest_peak.py
+++ 3.algorithmic_expert/Arrays/12.longest_peak.py
@@ -25,5 +25,24 @@ Detailed explanation of the Solution:
 ####################################
 
 def longestPeak(array):
-    # Write your code here.
-    pass
+    longestPeakLength=0
+    i=1
+    while i<len(array) - 1:
+        isPeak=array[i- 1]<array[i]and array[i]>array[i+1]
+        if not isPeak:
+            i+=1
+            continue
+
+        leftIdx =i - 2
+        while leftIdx>=0 and array[leftIdx] < array[leftIdx+1]:
+            leftIdx -=1
+        rightIdx = i+2
+        while rightIdx < len(array)and array[rightIdx] < array[rightIdx -1]:
+            rightIdx+=1
+
+        currentPeakLength = rightIdx - leftIdx - 1
+        longestPeakLength = max(longestPeakLength,currentPeakLength)
+        i=rightIdx
+    return longestPeakLength
+
+print(longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]))
