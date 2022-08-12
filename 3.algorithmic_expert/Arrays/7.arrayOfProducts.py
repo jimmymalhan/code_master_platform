f184d0479f592360267ad0060235498283960307
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-08-12 08:07:31 -0700

    chore: adding data

--- 3.algorithmic_expert/Arrays/7.arrayOfProducts.py
+++ 3.algorithmic_expert/Arrays/7.arrayOfProducts.py
@@ -1,61 +1,53 @@
-class Solution:
-    def __init__(self, array):
-        self.array = array
 
-    # O(n^2) time | O(n) space - where n in the length of the array
-    def arrayOfProducts1(self):
-        products = [1 for _ in range(len(self.array))]
+# O(n^2) time | O(n) space - where n in the length of the array
+def arrayOfProducts1(array):
+    products = [1 for _ in range(len(array))]
 
-        for i in range(len(self.array)):
-            runningProduct = 1
-            for j in range(len(self.array)):
-                if i != j:
-                    runningProduct *= self.array[j]
-            products[i] = runningProduct
-        return products
+    for i in range(len(array)):
+        runningProduct = 1
+        for j in range(len(array)):
+            if i != j:
+                runningProduct *= array[j]
+        products[i] = runningProduct
+    return products
+
+# O(n) time | O(n) space - where n in the length of the array
+def arrayOfProducts2(array):
+    products = [1 for _ in range(len(array))]
+    leftProducts = [1 for _ in range(len(array))]
+    rightProducts = [1 for _ in range(len(array))]
+
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        leftProducts[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
     
-    # O(n) time | O(n) space - where n in the length of the array
-    def arrayOfProducts2(self):
-        products = [1 for _ in range(len(self.array))]
-        leftProducts = [1 for _ in range(len(self.array))]
-        rightProducts = [1 for _ in range(len(self.array))]
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        rightProducts[i] *= rightRunningProduct
+        rightRunningProduct *= array[i]
+    
+    for i in range(len(array)):
+        products[i] = leftProducts[i] * rightProducts[i]
+    
+    return products
 
-        leftRunningProduct = 1
-        for i in range(len(self.array)):
-            leftProducts[i] = leftRunningProduct
-            leftRunningProduct *= self.array[i]
-        
-        rightRunningProduct = 1
-        for i in reversed(range(len(self.array))):
-            rightProducts[i] *= rightRunningProduct
-            rightRunningProduct *= self.array[i]
-        
-        for i in range(len(self.array)):
-            products[i] = leftProducts[i] * rightProducts[i]
-        
-        return products
+# O(n) time | O(n) space - where n in the length of the array
+def arrayOfProducts3(array):
+    products = [1 for _ in range(len(array))]
 
-    # O(n) time | O(n) space - where n in the length of the array
-    def arrayOfProducts3(self):
-        products = [1 for _ in range(len(self.array))]
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        products[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+    
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        products[i] *= rightRunningProduct
+        rightRunningProduct *= array[i]
+    
+    return products
 
-        leftRunningProduct = 1
-        for i in range(len(self.array)):
-            products[i] = leftRunningProduct
-            leftRunningProduct *= self.array[i]
-        
-        rightRunningProduct = 1
-        for i in reversed(range(len(self.array))):
-            products[i] *= rightRunningProduct
-            rightRunningProduct *= self.array[i]
-        
-        return products
-
-def main():
-    givenArray = Solution([5,1,4,2])
-    # print(givenArray.arrayOfProducts1())
-    # print(givenArray.arrayOfProducts2())
-    print(givenArray.arrayOfProducts3())
-
-if __name__ == '__main__':
-    main()
+print(arrayOfProducts1([5, 1, 4, 2])) # [8, 40, 10, 20]
+print(arrayOfProducts2([5, 1, 4, 2])) # [8, 40, 10, 20]
+print(arrayOfProducts3([5, 1, 4, 2])) # [8, 40, 10, 20]
