f7dd565c36936e36a05c147378550d7cdc29c786
Jimmy Malhan <jimmymalhan999@gmail.com>
2023-10-15 21:42:26 -0700

    working code in modular format

--- new_project/calculate_sessions.py
+++ new_project/calculate_sessions.py
@@ -1,47 +1,52 @@
 from collections import defaultdict
 from itertools import groupby
 
-class SessionCalculator:
-    def __init__(self, events):
-        self.events = events
+# Function to calculate sessions
+def calculate_sessions(sorted_events):
+    """
+    Calculate sessions based on sorted events.
 
-    def calculate_sessions(self):
-        sorted_events = sorted(self.events, key=lambda x: (x['visitorId'], x['timestamp']))
-        sessions_by_user = defaultdict(list)
+    Args:
+        sorted_events (list): List of events sorted by visitorId and timestamp.
 
-        for visitor_id, events in groupby(sorted_events, key=lambda x: x['visitorId']):
-            events = list(events)
-            current_session = {
-                "duration": 0,
-                "pages": [],
-                "startTime": events[0]['timestamp']
-            }
+    Returns:
+        dict: A dictionary containing sessions by user.
+    """
+    sessions_by_user = defaultdict(list)
 
-            for i, event in enumerate(events):
-                timestamp = event["timestamp"]
-                url = event["url"]
+    for visitor_id, events in groupby(sorted_events, key=lambda x: x['visitorId']):
+        events = list(events)
+        current_session = {
+            "duration": 0,
+            "pages": [],
+            "startTime": events[0]['timestamp']
+        }
+        
+        for i, event in enumerate(events):
+            timestamp = event["timestamp"]
+            url = event["url"]
+            
+            if i == 0:
+                current_session["pages"].append(url)
+                continue
 
-                if i == 0:
-                    current_session["pages"].append(url)
-                    continue
-
-                time_diff = timestamp - current_session["startTime"]
-                last_event_time = events[i - 1]['timestamp']
-
-                # Check if the event should be part of the current session
-                if timestamp - last_event_time <= 600000:  # 10 minutes in milliseconds
-                    current_session["duration"] = time_diff
-                    current_session["pages"].append(url)
-                else:
-                    # Finalize the current session and start a new one
-                    sessions_by_user[visitor_id].append(current_session)
-                    current_session = {
-                        "duration": 0,
-                        "pages": [url],
-                        "startTime": timestamp
-                    }
-
-            # Add the last session
-            sessions_by_user[visitor_id].append(current_session)
-
-        return {"sessionsByUser": dict(sessions_by_user)}
+            time_diff = timestamp - current_session["startTime"]
+            last_event_time = events[i - 1]['timestamp']
+            
+            # Check if the event should be part of the current session
+            if timestamp - last_event_time <= 600000:  # 10 minutes in milliseconds
+                current_session["duration"] = time_diff
+                current_session["pages"].append(url)
+            else:
+                # Finalize the current session and start a new one
+                sessions_by_user[visitor_id].append(current_session)
+                current_session = {
+                    "duration": 0,
+                    "pages": [url],
+                    "startTime": timestamp
+                }
+        
+        # Add the last session
+        sessions_by_user[visitor_id].append(current_session)
+        
+    return {"sessionsByUser": dict(sessions_by_user)}
