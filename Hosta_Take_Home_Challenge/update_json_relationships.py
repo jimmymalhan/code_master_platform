4d5bc4d33ecf49a62eb9cd6f2ce01dfdf7ccf336
Jimmy Malhan <jimmymalhan999@gmail.com>
2023-12-10 22:11:03 -0800

    fix: working code

--- Hosta_Take_Home_Challenge/update_json_relationships.py
+++ Hosta_Take_Home_Challenge/update_json_relationships.py
@@ -8,23 +8,24 @@ Tasks:
 3. Update JSON files with correct parent IDs based on CSV data.
 4. Ensure the code is reusable and well-documented.
 
-Author: [Your Name]
-Date: [Date]
+Author: Jimmy Malhan
+Date: 12/10/2023
 """
 
 import json
 import csv
+import chardet # pip install chardet
 
 class JSONFileHandler:
-    """
-    Handles operations related to JSON file processing.
-    """
     def __init__(self, filepath):
         self.filepath = filepath
         self.data = self.read_json()
 
     def read_json(self):
-        # Read JSON file and return data
+        """
+        Read a JSON file and return its contents as a dictionary.
+        Handles IOError if file reading fails.
+        """
         try:
             with open(self.filepath, 'r') as file:
                 return json.load(file)
@@ -34,9 +35,10 @@ class JSONFileHandler:
 
     def update_relationships(self, parent_mapping, image_id_mapping):
         """
-        Update the JSON data with new parent-child relationships.
+        Update JSON data with new parent-child relationships.
         :param parent_mapping: Dictionary with child IDs as keys and parent IDs as values.
         :param image_id_mapping: Dictionary mapping image IDs to unique object IDs.
+        :return: True if updates were made, False otherwise.
         """
         updated = False
         for obj in self.data.get('ops_3d', []):
@@ -50,33 +52,56 @@ class JSONFileHandler:
         return updated
 
     def save_json(self, new_filepath):
-        # Save the updated JSON data to a new file
+        """
+        Save the updated JSON data to a new file.
+        :param new_filepath: Path to the new JSON file.
+        """
         try:
             with open(new_filepath, 'w') as file:
                 json.dump(self.data, file, indent=4)
+            print(f"Updated JSON file saved as {new_filepath}.")
         except IOError as e:
             print(f"Error writing file {new_filepath}: {e}")
 
 class CSVFileHandler:
-    """
-    Handles operations related to CSV file processing.
-    """
     def __init__(self, filepath):
         self.filepath = filepath
 
     def read_csv(self):
-        # Read CSV file and return data
+        # Detect the encoding of the CSV file
+        encoding = self.detect_encoding()
+        if encoding is None:
+            print(f"Failed to detect encoding for file {self.filepath}")
+            return []
+
         try:
-            with open(self.filepath, newline='', encoding='utf-8-sig') as csvfile:  # Adjusted for potential UTF encoding with BOM
-                return list(csv.DictReader(csvfile))
+            with open(self.filepath, newline='', encoding=encoding) as csvfile:
+                csv_reader = csv.DictReader(csvfile, delimiter='\t')  # Adjust delimiter if necessary
+                return list(csv_reader)
         except IOError as e:
             print(f"Error reading file {self.filepath}: {e}")
             return []
+
+    def detect_encoding(self):
+        try:
+            with open(self.filepath, 'rb') as file:
+                detector = chardet.universaldetector.UniversalDetector()
+                for line in file:
+                    detector.feed(line)
+                    if detector.done:
+                        break
+                detector.close()
+                return detector.result['encoding']
+        except IOError as e:
+            print(f"Error reading file {self.filepath}: {e}")
+            return None
 
 def find_parent_child_relationships(csv_data):
     """
     Analyze CSV data to find parent-child relationships.
     Returns two dictionaries: one mapping child IDs to parent IDs, and another mapping image IDs to unique object IDs.
+    :param csv_data: List of dictionaries representing CSV data.
+    :return: Tuple of dictionaries (parent_mapping, image_id_mapping).
     """
     parent_mapping = {}
     image_id_mapping = {}
@@ -88,8 +113,12 @@ def find_parent_child_relationships(csv_data):
         for key, value in row.items():
             if 'Image' in key and value:
                 image_id_mapping[value] = object_id
-    return parent_mapping, image_id_mapping
 
+    # Debugging: Print the mappings
+    print("Parent Mapping:", parent_mapping)
+    print("Image ID Mapping:", image_id_mapping)
+
+    return parent_mapping, image_id_mapping
 def main():
     # Main function to orchestrate the updating process
     json_file_paths = [
@@ -101,20 +130,23 @@ def main():
 
     csv_handler = CSVFileHandler(csv_file_path)
     csv_data = csv_handler.read_csv()
-    relationships = find_parent_child_relationships(csv_data)
+    parent_mapping, image_id_mapping = find_parent_child_relationships(csv_data)
 
     for json_path in json_file_paths:
         json_handler = JSONFileHandler(json_path)
         if json_handler.data and csv_data:
-            updated = json_handler.update_relationships(relationships)
+            print(f"Processing JSON file: {json_path}")
+            updated = json_handler.update_relationships(parent_mapping, image_id_mapping)  # Corrected: Pass both mappings
             if updated:
-                new_json_path = f"updated_{json_path}"  # Naming the new file
+                new_json_path = f"updated_{json_path}"
                 json_handler.save_json(new_json_path)
                 print(f"Updated JSON file saved as {new_json_path}.")
             else:
                 print(f"No updates were made to the JSON file {json_path}.")
         else:
             print(f"Failed to read data from {json_path} or CSV file.")
+
+
 
 if __name__ == "__main__":
     main()
